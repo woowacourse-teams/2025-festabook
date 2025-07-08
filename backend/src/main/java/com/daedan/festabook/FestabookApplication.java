cd4cc6fc96306cbfcc91979a37b9113fd976535b
soeun2537 <soeun2537@gmail.com>
2025-07-08 19:44:49 +0900

    init: 백엔드 개발 환경 초기 세팅

---
+++ backend/src/main/java/com/daedan/festabook/FestabookApplication.java
@@ -0,0 +1,13 @@
+package com.daedan.festabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestabookApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/daedan/festabook/FestabookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daedan.festabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestabookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
