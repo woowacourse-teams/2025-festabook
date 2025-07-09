f790097559c06157a314f3ad42ea7d64d3ca2717
changuii <rhljh7410@gmail.com>
2025-07-09 23:52:24 +0900

    test: 변경사항에 맞게 테스트 수정, 전체 조회 검증 추가

--- backend/src/test/java/com/daedan/festabook/announcement/service/AnnouncementServiceTest.java
+++ backend/src/test/java/com/daedan/festabook/announcement/service/AnnouncementServiceTest.java
@@ -1,16 +1,15 @@
 package com.daedan.festabook.announcement.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 import com.daedan.festabook.announcement.domain.Announcement;
-import com.daedan.festabook.announcement.domain.DateTimeGenerator;
 import com.daedan.festabook.announcement.dto.AnnouncementRequest;
 import com.daedan.festabook.announcement.dto.AnnouncementResponse;
 import com.daedan.festabook.announcement.infrastructure.AnnouncementJpaRepository;
-import java.time.LocalDate;
-import java.time.LocalTime;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -18,13 +17,11 @@ import org.junit.jupiter.api.Test;
 class AnnouncementServiceTest {
 
     private final AnnouncementJpaRepository announcementJpaRepository;
-    private final DateTimeGenerator dateTimeGenerator;
     private final AnnouncementService announcementService;
 
     public AnnouncementServiceTest() {
         this.announcementJpaRepository = mock(AnnouncementJpaRepository.class);
-        this.dateTimeGenerator = mock(DateTimeGenerator.class);
-        this.announcementService = new AnnouncementService(announcementJpaRepository, dateTimeGenerator);
+        this.announcementService = new AnnouncementService(announcementJpaRepository);
     }
 
     @Nested
@@ -38,27 +35,20 @@ class AnnouncementServiceTest {
                     "잠시후, ~~~에서 재학생존 스탬프 배부를 시작하겠습니다."
             );
 
-            LocalDate date = LocalDate.of(2025, 7, 9);
-            LocalTime time = LocalTime.of(16, 25);
+            LocalDateTime createAt = LocalDateTime.of(2025, 7, 9, 16, 25, 0);
             Announcement notSaved = Announcement.builder()
                     .title(request.title())
-                    .date(date)
-                    .time(time)
+                    .createAt(createAt)
                     .content(request.content())
                     .build();
-            Announcement saved = new Announcement(1L, request.title(), date, time, request.content());
+            Announcement saved = new Announcement(1L, request.title(), createAt, request.content());
             given(announcementJpaRepository.save(notSaved))
                     .willReturn(saved);
-            given(dateTimeGenerator.generateDate())
-                    .willReturn(date);
-            given(dateTimeGenerator.generateTime())
-                    .willReturn(time);
 
             AnnouncementResponse expected = new AnnouncementResponse(
                     1L,
                     request.title(),
-                    date,
-                    time,
+                    createAt,
                     request.content()
             );
 
@@ -77,13 +67,12 @@ class AnnouncementServiceTest {
         void 공지를_정상적으로_전체_조회한다() {
             // given
             String title = "공지 제목";
-            LocalDate date = LocalDate.of(2025, 7, 9);
-            LocalTime time = LocalTime.of(16, 25);
+            LocalDateTime createAt = LocalDateTime.of(2025, 7, 9, 16, 25, 0);
             String content = "공지 내용";
 
-            Announcement announcement1 = new Announcement(1L, title, date, time, content);
-            Announcement announcement2 = new Announcement(2L, title, date, time, content);
-            Announcement announcement3 = new Announcement(3L, title, date, time, content);
+            Announcement announcement1 = new Announcement(1L, title, createAt, content);
+            Announcement announcement2 = new Announcement(2L, title, createAt, content);
+            Announcement announcement3 = new Announcement(3L, title, createAt, content);
             given(announcementJpaRepository.findAll())
                     .willReturn(List.of(announcement1, announcement2, announcement3));
 
@@ -91,7 +80,12 @@ class AnnouncementServiceTest {
             List<AnnouncementResponse> actual = announcementService.findAllAnnouncement();
 
             // then
-            assertThat(actual).hasSize(3);
+            assertSoftly(s -> {
+                s.assertThat(actual).hasSize(3);
+                s.assertThat(actual.get(0).id()).isEqualTo(1L);
+                s.assertThat(actual.get(1).id()).isEqualTo(2L);
+                s.assertThat(actual.get(2).id()).isEqualTo(3L);
+            });
         }
     }
 }
