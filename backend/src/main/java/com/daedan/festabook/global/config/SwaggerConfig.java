affb94692e45a03cf3bd1b84005d878d3d98736c
taek2222 <oot309@daum.net>
2025-07-09 17:55:19 +0900

    feat: build 버전 정보 기입 수정

--- backend/src/main/java/com/daedan/festabook/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/daedan/festabook/global/config/SwaggerConfig.java
@@ -2,11 +2,16 @@ package com.daedan.festabook.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class SwaggerConfig {
+
+    private final BuildProperties buildProperties;
 
     @Bean
     public OpenAPI openAPI() {
@@ -18,6 +23,6 @@ public class SwaggerConfig {
         return new Info()
                 .title("FestaBook API")
                 .description("FestaBook API")
-                .version("0.0.0");
+                .version(buildProperties.getVersion());
     }
 }
