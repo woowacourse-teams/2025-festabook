92857109dbffa176cbb7cb0937c46470e26746f1
soeun2537 <soeun2537@gmail.com>
2025-07-09 21:56:15 +0900

    feat: PlaceRequest Swagger 설정

--- backend/src/main/java/com/daedan/festabook/place/dto/PlaceRequest.java
+++ backend/src/main/java/com/daedan/festabook/place/dto/PlaceRequest.java
@@ -6,13 +6,25 @@ import java.time.LocalTime;
 
 public record PlaceRequest(
 
-        @Schema(description = "제목", example = "")
+        @Schema(description = "제목", example = "코딩하며 한잔")
         String title,
+
+        @Schema(description = "설명", example = "시원한 맥주와 맛있는 치킨!")
         String description,
+
+        @Schema(description = "카테고리", example = "FOOD_TRUCK")
         PlaceCategory category,
+
+        @Schema(description = "장소", example = "공학관 앞")
         String location,
+
+        @Schema(description = "주최", example = "C블C블")
         String host,
+
+        @Schema(description = "운영 시작 시간", example = "09:00")
         LocalTime startTime,
+
+        @Schema(description = "운영 마감 시간", example = "18:00")
         LocalTime endTime
 ) {
 }
