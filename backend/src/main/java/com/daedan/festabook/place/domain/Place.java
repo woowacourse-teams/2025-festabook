1706d0a3a7af4c11e27c11944a805c14de1320dd
soeun2537 <soeun2537@gmail.com>
2025-07-09 21:56:29 +0900

    refactor: 어노테이션 순서 변경

--- backend/src/main/java/com/daedan/festabook/place/domain/Place.java
+++ backend/src/main/java/com/daedan/festabook/place/domain/Place.java
@@ -21,8 +21,8 @@ import lombok.NoArgsConstructor;
 public class Place {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
