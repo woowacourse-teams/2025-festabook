355069cbf2aaa9b68e0e2b9457163dc9103d0b75
soeun2537 <soeun2537@gmail.com>
2025-07-10 16:12:24 +0900

    feat: Place 엔드포인트 구현

--- backend/src/main/java/com/daedan/festabook/place/controller/PlaceController.java
+++ backend/src/main/java/com/daedan/festabook/place/controller/PlaceController.java
@@ -1,6 +1,10 @@
 package com.daedan.festabook.place.controller;
 
+import com.daedan.festabook.place.dto.PlaceAnnouncementResponses;
+import com.daedan.festabook.place.dto.PlaceImageResponses;
 import com.daedan.festabook.place.dto.PlaceResponses;
+import com.daedan.festabook.place.service.PlaceAnnouncementService;
+import com.daedan.festabook.place.service.PlaceImageService;
 import com.daedan.festabook.place.service.PlaceService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -9,6 +13,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class PlaceController {
 
     private final PlaceService placeService;
+    private final PlaceAnnouncementService placeAnnouncementService;
+    private final PlaceImageService placeImageService;
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
@@ -29,5 +36,29 @@ public class PlaceController {
     })
     public PlaceResponses findAllPlace() {
         return placeService.findAllPlace();
+    }
+
+    @GetMapping("/{placeId}/announcements")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "특정 플레이스의 모든 공지 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", useReturnTypeSchema = true),
+    })
+    public PlaceAnnouncementResponses findAllPlaceAnnouncementByPlaceId(
+            @PathVariable Long placeId
+    ) {
+        return placeAnnouncementService.findAllPlaceAnnouncementByPlaceId(placeId);
+    }
+
+    @GetMapping("/{placeId}/images")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "특정 플레이스의 모든 이미지 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", useReturnTypeSchema = true),
+    })
+    public PlaceImageResponses findAllPlaceImageByPlaceId(
+            @PathVariable Long placeId
+    ) {
+        return placeImageService.findAllPlaceImageByPlaceId(placeId);
     }
 }
