bbbcd461bcbbffc0c2ffe179a94c2b2b3f023f30
soeun2537 <soeun2537@gmail.com>
2025-07-10 09:37:04 +0900

    refactor: Enum 문자열 저장 방식으로 변경

--- backend/src/main/java/com/daedan/festabook/place/domain/Place.java
+++ backend/src/main/java/com/daedan/festabook/place/domain/Place.java
@@ -2,6 +2,8 @@ package com.daedan.festabook.place.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -32,6 +34,7 @@ public class Place {
     private String description;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private PlaceCategory category;
 
     @Column(nullable = false)
