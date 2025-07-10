64bd97859ec7199a0c81b135ebcef854d2268b61
changuii <rhljh7410@gmail.com>
2025-07-10 15:49:09 +0900

    refactor: 첫줄 개행

--- backend/src/main/java/com/daedan/festabook/global/exception/BusinessException.java
+++ backend/src/main/java/com/daedan/festabook/global/exception/BusinessException.java
@@ -3,6 +3,7 @@ package com.daedan.festabook.global.exception;
 import org.springframework.http.HttpStatus;
 
 public class BusinessException extends RuntimeException {
+
     private final HttpStatus status;
 
     public BusinessException(String message, HttpStatus status) {
