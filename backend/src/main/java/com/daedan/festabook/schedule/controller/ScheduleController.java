9df6420e212afef3f3cef54bd00d165a58dea16b
taek2222 <oot309@daum.net>
2025-07-11 17:44:57 +0900

    style: Status 컨벤션 적용

--- backend/src/main/java/com/daedan/festabook/schedule/controller/ScheduleController.java
+++ backend/src/main/java/com/daedan/festabook/schedule/controller/ScheduleController.java
@@ -4,9 +4,11 @@ import com.daedan.festabook.schedule.dto.EventDayResponses;
 import com.daedan.festabook.schedule.dto.EventResponses;
 import com.daedan.festabook.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,11 +19,13 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public EventDayResponses getEventDays() {
         return scheduleService.getEventDays();
     }
 
     @GetMapping("/{eventDayId}")
+    @ResponseStatus(HttpStatus.OK)
     public EventResponses getEvents(
             @PathVariable Long eventDayId
     ) {
