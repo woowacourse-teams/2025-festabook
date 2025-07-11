9e536e99622062595356a6acd30ca0ca044302a3
taek2222 <oot309@daum.net>
2025-07-11 20:20:20 +0900

    refactor: LAZY 로딩 추가

--- backend/src/main/java/com/daedan/festabook/schedule/domain/Event.java
+++ backend/src/main/java/com/daedan/festabook/schedule/domain/Event.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -38,8 +39,8 @@ public class Event {
     @Column(nullable = false)
     private String location;
 
-    @ManyToOne
     @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
     private EventDay eventDay;
 
     public Event(
