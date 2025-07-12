0ebee083aa642af6833c68be7439f942d1368193
박지민 <pjm5489@naver.com>
2025-07-12 17:38:23 +0900

    feat: FragmentContainerView 추가
    - MainActivity에 FragmentContainerView를 추가하여 Fragment를 표시할 수 있도록 함
    - BottomNavigationView의 아이템 클릭에 따라 HomeFragment, ScheduleFragment 등으로 전환하는 로직 구현
    - HomeFragment 및 HomeFragment 레이아웃 파일 추가

--- android/app/src/main/java/com/daedan/festabook/MainActivity.kt
+++ android/app/src/main/java/com/daedan/festabook/MainActivity.kt
@@ -5,10 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import com.daedan.festabook.databinding.ActivityMainBinding
+import com.daedan.festabook.presentation.schedule.ScheduleFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private val homeFragment by lazy {
+        HomeFragment().newInstance()
+    }
+
+    private val scheduleFragment by lazy {
+        ScheduleFragment().newInstance()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +31,56 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                add(R.id.fcv_fragment_container, homeFragment, TAG_HOME_FRAGMENT)
+            }
+        }
+        onClickBottomNavigationBarItem()
+    }
+
+    private fun onClickBottomNavigationBarItem() {
+        binding.bnvMenu.setOnItemSelectedListener { icon ->
+
+            if (binding.bnvMenu.selectedItemId == icon.itemId) {
+                return@setOnItemSelectedListener false
+            }
+            when (icon.itemId) {
+                R.id.item_menu_home -> switchFragment(homeFragment, TAG_HOME_FRAGMENT)
+                R.id.item_menu_schedule -> switchFragment(scheduleFragment, TAG_SCHEDULE_FRAGMENT)
+                R.id.item_menu_map -> {}
+                R.id.item_menu_news -> {}
+                R.id.item_menu_setting -> {}
+            }
+            true
+        }
+    }
+
+    private fun switchFragment(
+        fragment: Fragment,
+        tag: String,
+    ) {
+        supportFragmentManager.commit {
+            supportFragmentManager.fragments.forEach { fragment -> hide(fragment) }
+
+            val existing = supportFragmentManager.findFragmentByTag(tag)
+            if (existing != null) {
+                show(existing)
+            } else {
+                add(R.id.fcv_fragment_container, fragment, tag)
+            }
+            setReorderingAllowed(true)
+        }
+    }
+
+    companion object {
+        private const val TAG_HOME_FRAGMENT = "homeFragment"
+        private const val TAG_SCHEDULE_FRAGMENT = "scheduleFragment"
+
+        fun Fragment.newInstance(): Fragment =
+            this.apply {
+                arguments = Bundle()
+            }
     }
 }
