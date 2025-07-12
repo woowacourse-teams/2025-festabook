43f0f881553267e4298b4f41f7f53a675c3ad59f
taek2222 <oot309@daum.net>
2025-07-12 17:05:23 +0900

    feat: API Group 추가

--- backend/src/main/java/com/daedan/festabook/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/daedan/festabook/global/config/SwaggerConfig.java
@@ -2,7 +2,9 @@ package com.daedan.festabook.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +13,28 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class SwaggerConfig {
 
+    private static final List<String[]> API_GROUP = List.of(
+            new String[]{"일정 API", "/schedules/**"},
+            new String[]{"플레이스 API", "/places/**"},
+            new String[]{"공지 API", "/announcements/**"}
+    );
+
     private final BuildProperties buildProperties;
 
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI openApi() {
         return new OpenAPI()
                 .info(createInfo());
+    }
+
+    @Bean
+    public List<GroupedOpenApi> apis() {
+        return API_GROUP.stream()
+                .map(group -> GroupedOpenApi.builder()
+                        .group(group[0])
+                        .pathsToMatch(group[1])
+                        .build())
+                .toList();
     }
 
     private Info createInfo() {
