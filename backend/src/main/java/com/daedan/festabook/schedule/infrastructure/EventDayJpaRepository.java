36aef3b7e4de72854f492308dd3f73315801b8e7
taek2222 <oot309@daum.net>
2025-07-12 16:16:06 +0900

    style: repository -> infrastructure 네이밍 변경

--- backend/src/main/java/com/daedan/festabook/schedule/infrastructure/EventDayJpaRepository.java
+++ backend/src/main/java/com/daedan/festabook/schedule/infrastructure/EventDayJpaRepository.java
@@ -1,4 +1,4 @@
-package com.daedan.festabook.schedule.repository;
+package com.daedan.festabook.schedule.infrastructure;
 
 import com.daedan.festabook.schedule.domain.EventDay;
 import org.springframework.data.jpa.repository.JpaRepository;
--- backend/src/main/java/com/daedan/festabook/schedule/infrastructure/EventJpaRepository.java
+++ backend/src/main/java/com/daedan/festabook/schedule/infrastructure/EventJpaRepository.java
@@ -1,4 +1,4 @@
-package com.daedan.festabook.schedule.repository;
+package com.daedan.festabook.schedule.infrastructure;
 
 import com.daedan.festabook.schedule.domain.Event;
 import java.util.List;
