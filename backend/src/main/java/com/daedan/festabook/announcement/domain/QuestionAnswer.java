7253de21fd2299984f12a1db0e282bfcdf9b4cf9
eoehd1ek <eoehd2ek@gmail.com>
2025-07-13 23:53:34 +0900

    feat: QuestionAnswer 도메인 추가

---
+++ backend/src/main/java/com/daedan/festabook/announcement/domain/QuestionAnswer.java
@@ -0,0 +1,48 @@
+package com.daedan.festabook.announcement.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class QuestionAnswer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String question;
+
+    @Column(nullable = false)
+    private String answer;
+
+    @CreatedDate
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    public QuestionAnswer(
+            String title,
+            String question,
+            String answer
+    ) {
+        this.title = title;
+        this.question = question;
+        this.answer = answer;
+    }
+}
