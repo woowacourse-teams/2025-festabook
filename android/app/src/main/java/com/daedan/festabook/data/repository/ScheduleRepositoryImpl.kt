b0c7d07654ef2aef220f2ddce7f9ae699cb4d7ec
박지민 <pjm5489@naver.com>
2025-07-14 17:35:05 +0900

    refactor: `ScheduleRepositoryImpl` 에서 임시 데이터의 `description`, `isBookmarked` 필드 제거
    임시 데이터에서 더 이상 사용하지 않는 `description`과 `isBookmarked` 필드를 제거

--- android/app/src/main/java/com/daedan/festabook/data/repository/ScheduleRepositoryImpl.kt
+++ android/app/src/main/java/com/daedan/festabook/data/repository/ScheduleRepositoryImpl.kt
@@ -14,9 +14,7 @@ class ScheduleRepositoryImpl : ScheduleRepository {
                 startTime = "09:00",
                 endTime = "10:00",
                 title = "개막식",
-                description = "축제의 시작을 알리는 개막식입니다.",
                 location = "운동장",
-                isBookmarked = false,
             ),
             ScheduleEvent(
                 id = 2L,
@@ -25,9 +23,7 @@ class ScheduleRepositoryImpl : ScheduleRepository {
                 startTime = "10:30",
                 endTime = "11:30",
                 title = "버스킹 공연",
-                description = "동아리의 자유로운 버스킹 무대.",
                 location = "중앙 광장",
-                isBookmarked = true,
             ),
             ScheduleEvent(
                 id = 3L,
@@ -36,9 +32,7 @@ class ScheduleRepositoryImpl : ScheduleRepository {
                 startTime = "12:00",
                 endTime = "13:00",
                 title = "푸드트럭 런치타임",
-                description = "다양한 먹거리를 즐겨보세요.",
                 location = "푸드트럭 존",
-                isBookmarked = false,
             ),
             ScheduleEvent(
                 id = 4L,
@@ -47,9 +41,7 @@ class ScheduleRepositoryImpl : ScheduleRepository {
                 startTime = "13:30",
                 endTime = "14:30",
                 title = "게임 이벤트",
-                description = "재미있는 미니게임 대회!",
                 location = "이벤트 홀",
-                isBookmarked = true,
             ),
             ScheduleEvent(
                 id = 5L,
@@ -58,9 +50,7 @@ class ScheduleRepositoryImpl : ScheduleRepository {
                 startTime = "15:00",
                 endTime = "16:30",
                 title = "학생회 토크쇼",
-                description = "학생회와 함께하는 소통의 시간.",
                 location = "대강당",
-                isBookmarked = true,
             ),
             ScheduleEvent(
                 id = 6L,
@@ -69,9 +59,7 @@ class ScheduleRepositoryImpl : ScheduleRepository {
                 startTime = "17:00",
                 endTime = "18:00",
                 title = "폐막식",
-                description = "축제의 마지막을 장식하는 시간.",
                 location = "운동장",
-                isBookmarked = false,
             ),
         )
 }
