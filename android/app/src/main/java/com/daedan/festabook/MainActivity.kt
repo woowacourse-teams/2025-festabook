f850a314cd6b2f251d019d43335bcb9abbd15e30
oungsi2000 <oungsi1000@gmail.com>
2025-07-14 16:50:53 +0900

    feat: 메인액티비티에서 프래그먼트 연결

--- android/app/src/main/java/com/daedan/festabook/MainActivity.kt
+++ android/app/src/main/java/com/daedan/festabook/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.daedan.festabook.databinding.ActivityMainBinding
+import com.daedan.festabook.presentation.placeList.PlaceListFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -20,5 +21,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val fragment = PlaceListFragment()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fl_container, fragment);
+        fragmentTransaction.commit();
+
     }
 }
