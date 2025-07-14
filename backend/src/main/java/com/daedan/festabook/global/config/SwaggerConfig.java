3ae626a78524d904de65f951980342aaf3ea7d3a
taek2222 <oot309@daum.net>
2025-07-14 16:13:06 +0900

    feat: Swagger OrganizationId 헤더 삽입 방법 추가

--- backend/src/main/java/com/daedan/festabook/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/daedan/festabook/global/config/SwaggerConfig.java
@@ -1,7 +1,13 @@
 package com.daedan.festabook.global.config;
 
+import static io.swagger.v3.oas.models.security.SecurityScheme.In;
+
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
@@ -13,6 +19,7 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 public class SwaggerConfig {
 
+    private static final String ORGANIZATION_SCHEME_NAME = "조직(Organization) ID 값";
     private static final List<String[]> API_GROUPS = List.of(
             new String[]{"일정 API", "/schedules/**"},
             new String[]{"플레이스 API", "/places/**"},
@@ -24,7 +31,9 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openApi() {
         return new OpenAPI()
-                .info(createInfo());
+                .info(createInfo())
+                .addSecurityItem(createSecurityRequirement())
+                .components(createComponents());
     }
 
     @Bean
@@ -42,5 +51,17 @@ public class SwaggerConfig {
                 .title("FestaBook API")
                 .description("FestaBook API")
                 .version(buildProperties.getVersion());
+    }
+
+    private static SecurityRequirement createSecurityRequirement() {
+        return new SecurityRequirement().addList(ORGANIZATION_SCHEME_NAME);
+    }
+
+    private static Components createComponents() {
+        return new Components()
+                .addSecuritySchemes(ORGANIZATION_SCHEME_NAME, new SecurityScheme()
+                        .type(Type.APIKEY)
+                        .in(In.HEADER)
+                        .name("organization"));
     }
 }
