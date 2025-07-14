502f35141ee86f2393ab2f172d414ae4548afebd
changuii <rhljh7410@gmail.com>
2025-07-14 16:34:37 +0900

    test: organization 생성을 Fixture로 변경

--- backend/src/test/java/com/daedan/festabook/announcement/domain/AnnouncementFixture.java
+++ backend/src/test/java/com/daedan/festabook/announcement/domain/AnnouncementFixture.java
@@ -1,11 +1,11 @@
 package com.daedan.festabook.announcement.domain;
 
 import com.daedan.festabook.organization.domain.Organization;
+import com.daedan.festabook.organization.domain.OrganizationFixture;
 
 public class AnnouncementFixture {
 
-    // TODO: OrganizationFixture로 변경
-    private static final Organization DEFAULT_ORGANIZATION = new Organization("서울대학교");
+    private static final Organization DEFAULT_ORGANIZATION = OrganizationFixture.create();
     private static final String DEFAULT_TITLE = "폭우가 내립니다.";
     private static final String DEFAULT_CONTENT = "우산을 챙겨주세요.";
 
