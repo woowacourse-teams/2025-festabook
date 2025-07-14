08e9cfad544d429b24495ba5906d88814f6bbae3
changuii <rhljh7410@gmail.com>
2025-07-14 15:54:55 +0900

    test: TODO 추가

--- backend/src/test/java/com/daedan/festabook/announcement/domain/AnnouncementFixture.java
+++ backend/src/test/java/com/daedan/festabook/announcement/domain/AnnouncementFixture.java
@@ -4,6 +4,7 @@ import com.daedan.festabook.organization.domain.Organization;
 
 public class AnnouncementFixture {
 
+    // TODO: OrganizationFixture로 변경
     private static final Organization DEFAULT_ORGANIZATION = new Organization("서울대학교");
     private static final String DEFAULT_TITLE = "폭우가 내립니다.";
     private static final String DEFAULT_CONTENT = "우산을 챙겨주세요.";
