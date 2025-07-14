ccd8a9be4ec2a313621641cd3c9fa8f790238c04
박지민 <pjm5489@naver.com>
2025-07-14 16:25:21 +0900

    feat: ScheduleEvent 모델에 설명, 북마크 여부 추가 및 샘플 데이터 수정
    ScheduleEvent 데이터 클래스에 'description' (행사 설명)과 'isBookmarked' (북마크 여부) 필드를 추가
    이에 따라 ScheduleRepositoryImpl의 샘플 데이터를 새로운 필드를 포함하도록 수정하고, 기존 데이터의 시간, 내용 등을 일부 변경했=
    또한, 'status' 필드의 타입을 String에서 ScheduleEventStatus Enum으로 변경

--- android/app/src/main/java/com/daedan/festabook/domain/model/ScheduleEvent.kt
+++ android/app/src/main/java/com/daedan/festabook/domain/model/ScheduleEvent.kt
@@ -3,10 +3,11 @@ package com.daedan.festabook.domain.model
 data class ScheduleEvent(
     val id: Long,
     val eventDayId: Long,
-    val status: String,
+    val status: ScheduleEventStatus,
     val startTime: String,
     val endTime: String,
     val title: String,
+    val description: String,
     val location: String,
     val isBookmarked: Boolean = true,
 )
