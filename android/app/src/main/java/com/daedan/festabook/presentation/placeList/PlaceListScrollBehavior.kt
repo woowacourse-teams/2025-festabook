f0dbdcbcec38cb70f9bcd596770c6a34f9daae3a
oungsi2000 <oungsi1000@gmail.com>
2025-07-14 15:56:30 +0900

    feat: 장소 목록 스크롤 기능 구현
    - 장소 목록 스크롤 동작을 위한 `PlaceListScrollBehavior` 추가
    - `fragment_place_list.xml`에 `PlaceListScrollBehavior` 적용 및 초기/최소 Y값 설정
    - `AnimationUtil.kt`에 스크롤 애니메이션 유틸 함수 추가
    - `attrs.xml`에 `PlaceListScrollBehavior` 관련 속성 정의

---
+++ android/app/src/main/java/com/daedan/festabook/presentation/placeList/PlaceListScrollBehavior.kt
@@ -0,0 +1,120 @@
+package com.daedan.festabook.presentation.placeList
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.OvershootInterpolator
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.ViewCompat
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.daedan.festabook.R
+import com.daedan.festabook.databinding.ActivityMainBinding
+import com.daedan.festabook.databinding.FragmentPlaceListBinding
+import com.daedan.festabook.databinding.ItemPlaceListBinding
+import androidx.core.content.withStyledAttributes
+import com.daedan.festabook.presentation.common.scrollAnimation
+
+class PlaceListScrollBehavior(
+    val context: Context,
+    attrs: AttributeSet
+) : CoordinatorLayout.Behavior<ConstraintLayout>() {
+    private lateinit var binding:FragmentPlaceListBinding
+    private var initialY: Float = 0f
+    private var minimumY: Float = 0f
+    private var isInitialized = false
+
+    init {
+        context.withStyledAttributes(attrs, R.styleable.PlaceListScrollBehavior) {
+            initialY = getDimension(R.styleable.PlaceListScrollBehavior_initialY, 0f)
+            minimumY = getDimension(R.styleable.PlaceListScrollBehavior_minimumY, 0f)
+        }
+    }
+
+    override fun onLayoutChild(
+        parent: CoordinatorLayout,
+        child: ConstraintLayout,
+        layoutDirection: Int
+    ): Boolean {
+        if (!isInitialized) {
+            binding = DataBindingUtil.findBinding<FragmentPlaceListBinding>(parent)
+                ?:return super.onLayoutChild(parent, child, layoutDirection)
+            isInitialized = true
+            child.translationY = child.rootView.height - initialY
+        }
+        return super.onLayoutChild(parent, child, layoutDirection)
+    }
+
+    override fun onStartNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: ConstraintLayout,
+        directTargetChild: View,
+        target: View,
+        axes: Int,
+        type: Int
+    ): Boolean {
+        return axes == ViewCompat.SCROLL_AXIS_VERTICAL
+    }
+
+    override fun onNestedPreScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: ConstraintLayout,
+        target: View,
+        dx: Int,
+        dy: Int,
+        consumed: IntArray,
+        type: Int
+    ) {
+        super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+        //아래로 스크롤 하고, 리사이클러 뷰의 최상단에 도달하지 않았을 때
+        if (dy < 0 && binding.rvPlaces.canScrollVertically(-1)) {
+            child.background = AppCompatResources.getDrawable(context, R.drawable.bg_place_list)
+            consumed[1] = 0
+            return
+        }
+
+        val currentTranslationY = child.translationY
+        val newTranslationY = currentTranslationY - dy
+        val maxHeight = child.rootView.height.toFloat()
+        val limitedTranslationY = newTranslationY.coerceIn(0f, maxHeight - minimumY)
+        child.translationY = limitedTranslationY
+        child.scrollAnimation(limitedTranslationY)
+        consumed[1] = limitedTranslationY.toInt()
+    }
+
+    override fun onNestedScroll(
+        coordinatorLayout: CoordinatorLayout,
+        child: ConstraintLayout,
+        target: View,
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        type: Int,
+        consumed: IntArray
+    ) {
+        //리스트가 모두 펼쳐지면 모서리 부분을 없앱니다
+        if (dyUnconsumed == 0) {
+            child.background = ContextCompat.getColor(context, R.color.gray050).toDrawable()
+        }
+
+        super.onNestedScroll(
+            coordinatorLayout,
+            child,
+            target,
+            dxConsumed,
+            dyConsumed,
+            dxUnconsumed,
+            dyUnconsumed,
+            type,
+            consumed
+        )
+    }
+}
+
