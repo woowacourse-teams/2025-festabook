6d9be37bcd80e523cfe61336ff702e4ed0621c40
DongjooSEO <readyjoo24@gmail.com>
2025-07-15 11:18:37 +0900

    refactor: NoticeViewHolder 파일 분리 및 패키지 이동

--- android/app/src/main/java/com/daedan/festabook/presentation/news/notice/NewsFragment.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/news/notice/NewsFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import com.daedan.festabook.R
 import com.daedan.festabook.databinding.FragmentNewsBinding
 import com.daedan.festabook.presentation.common.BaseFragment
+import com.daedan.festabook.presentation.news.notice.adapter.NoticeAdapter
 
 class NewsFragment : BaseFragment<FragmentNewsBinding>(R.layout.fragment_news) {
     private val noticeAdapter: NoticeAdapter by lazy { NoticeAdapter() }
@@ -14,6 +15,8 @@ class NewsFragment : BaseFragment<FragmentNewsBinding>(R.layout.fragment_news) {
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.rvNoticeList.adapter = noticeAdapter
         val notices =
             listOf(
                 NoticeUiModel("제목1", "설명1", "2025-07-14T05:22:39.963Z"),
--- android/app/src/main/java/com/daedan/festabook/presentation/news/notice/adapter/NoticeAdapter.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/news/notice/adapter/NoticeAdapter.kt
@@ -1,26 +1,9 @@
-package com.daedan.festabook.presentation.news.notice
+package com.daedan.festabook.presentation.news.notice.adapter
 
-import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
-import androidx.recyclerview.widget.RecyclerView
-
-class NoticeViewHolder(
-    private val binding: ItemNoticeBinding,
-) : RecyclerView.ViewHolder(binding.root) {
-    fun bind(notice: NoticeUiModel) {
-        binding.notice = notice
-    }
-
-    companion object {
-        fun from(parent: ViewGroup): NoticeViewHolder {
-            val binding =
-                ItemNoticeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-            return NoticeViewHolder(binding)
-        }
-    }
-}
+import com.daedan.festabook.presentation.news.notice.NoticeUiModel
 
 class NoticeAdapter : ListAdapter<NoticeUiModel, NoticeViewHolder>(noticeDiffCallback) {
     override fun onCreateViewHolder(
