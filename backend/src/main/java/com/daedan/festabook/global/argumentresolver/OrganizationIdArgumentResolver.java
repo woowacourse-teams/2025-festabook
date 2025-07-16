9d3ebc297952d469c28ebe03186ce8ee4aeef9a0
taek2222 <oot309@daum.net>
2025-07-16 17:40:18 +0900

    feat: 조직 Id 예외 추가하고 커스텀 예외 등록

--- backend/src/main/java/com/daedan/festabook/global/argumentresolver/OrganizationIdArgumentResolver.java
+++ backend/src/main/java/com/daedan/festabook/global/argumentresolver/OrganizationIdArgumentResolver.java
@@ -1,6 +1,10 @@
 package com.daedan.festabook.global.argumentresolver;
 
+import com.daedan.festabook.global.exception.BusinessException;
+import com.daedan.festabook.organization.infrastructure.OrganizationJpaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -8,9 +12,11 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 @Component
+@RequiredArgsConstructor
 public class OrganizationIdArgumentResolver implements HandlerMethodArgumentResolver {
 
     private static final String ORGANIZATION_ID_HEADER = "organization";
+    private final OrganizationJpaRepository organizationJpaRepository;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
@@ -27,14 +33,24 @@ public class OrganizationIdArgumentResolver implements HandlerMethodArgumentReso
             WebDataBinderFactory binderFactory
     ) {
         String organizationId = webRequest.getHeader(ORGANIZATION_ID_HEADER);
-        validateNull(organizationId);
-        return Long.parseLong(organizationId);
+        return validateAndParseOrganizationId(organizationId);
     }
 
-    // TODO : 커스텀 예외 등록
-    private void validateNull(String organizationId) {
+    private Long validateAndParseOrganizationId(String organizationId) {
         if (organizationId == null) {
-            throw new RuntimeException();
+            throw new BusinessException("Organization 헤더가 누락되었습니다.", HttpStatus.FORBIDDEN);
         }
+
+        Long parsedId;
+        try {
+            parsedId = Long.parseLong(organizationId);
+        } catch (NumberFormatException e) {
+            throw new BusinessException("Organization 헤더의 값은 숫자여야 합니다.", HttpStatus.FORBIDDEN);
+        }
+
+        if (!organizationJpaRepository.existsById(parsedId)) {
+            throw new BusinessException("존재하지 않는 OrganizationId 입니다.", HttpStatus.FORBIDDEN);
+        }
+        return parsedId;
     }
 }
