6a89596c6b117128285a6570f043dce900adff2e
oungsi2000 <oungsi1000@gmail.com>
2025-07-16 17:36:07 +0900

    refactor: ViewPagerAdapter ListAdapter로

--- android/app/src/main/java/com/daedan/festabook/presentation/placeDetail/PlaceImageViewPagerAdapter.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/placeDetail/PlaceImageViewPagerAdapter.kt
@@ -1,22 +1,57 @@
 package com.daedan.festabook.presentation.placeDetail
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.daedan.festabook.databinding.ItemPlaceImageBinding
 
 class PlaceImageViewPagerAdapter(
     private val imageUrls: List<String>,
-) : RecyclerView.Adapter<PlaceImageViewPagerViewHolder>() {
+) : ListAdapter<String, PlaceImageViewPagerAdapter.ViewHolder>(DIFF_UTIL_CALLBACK) {
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
-    ): PlaceImageViewPagerViewHolder = PlaceImageViewPagerViewHolder.of(parent)
+    ): ViewHolder = ViewHolder.of(parent)
 
     override fun onBindViewHolder(
-        holder: PlaceImageViewPagerViewHolder,
+        holder: ViewHolder,
         position: Int,
     ) {
-        holder.bind(imageUrls[position])
+        holder.bind(getItem(position))
     }
 
     override fun getItemCount(): Int = imageUrls.size
+
+    class ViewHolder(
+        val binding: ItemPlaceImageBinding,
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(imageUrl: String) {
+            binding.imageUrl = imageUrl
+        }
+
+        companion object {
+            fun of(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemPlaceImageBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
+    }
+
+    companion object {
+        private val DIFF_UTIL_CALLBACK =
+            object : DiffUtil.ItemCallback<String>() {
+                override fun areItemsTheSame(
+                    oldItem: String,
+                    newItem: String,
+                ): Boolean = oldItem == newItem
+
+                override fun areContentsTheSame(
+                    oldItem: String,
+                    newItem: String,
+                ): Boolean = oldItem == newItem
+            }
+    }
 }
