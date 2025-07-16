eb46b15cb20fa53f92b8a605c095cdffe878b7da
soeun2537 <soeun2537@gmail.com>
2025-07-16 15:43:26 +0900

    docs: 주석 추가

--- backend/src/main/java/com/daedan/festabook/place/service/PlaceService.java
+++ backend/src/main/java/com/daedan/festabook/place/service/PlaceService.java
@@ -52,6 +52,7 @@ public class PlaceService {
         return PlaceResponse.from(place, placeImages, placeAnnouncements);
     }
 
+    // TODO: ExceptionHandler 등록 후 예외 변경
     private Place getPlaceById(Long placeId) {
         return placeJpaRepository.findById(placeId)
                 .orElseThrow(() -> new BusinessException("존재하지 않는 플레이스입니다.", HttpStatus.NOT_FOUND));
