a76ada6aa9ebd6554fdba651a07cbeff01733b3a
oungsi2000 <oungsi1000@gmail.com>
2025-07-16 11:14:42 +0900

    feat: 프래그먼트 전환 애니메이션 적용

--- android/app/src/main/java/com/daedan/festabook/presentation/common/AnimationUtil.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/common/AnimationUtil.kt
@@ -1,8 +1,10 @@
 package com.daedan.festabook.presentation.common
 
+import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateInterpolator
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 fun ViewGroup.scrollAnimation(limitedTranslationY: Float) {
     animate()
@@ -13,3 +15,22 @@ fun ViewGroup.scrollAnimation(limitedTranslationY: Float) {
 }
 
 fun RecyclerView.canScrollUp(): Boolean = this.canScrollVertically(-1)
+
+fun BottomNavigationView.animateHideBottomNavigationView() {
+    clearAnimation()
+    animate()
+        .translationY(height.toFloat())
+        .setDuration(200)
+        .withEndAction {
+            visibility = View.GONE
+        }.start()
+}
+
+fun BottomNavigationView.animateShowBottomNavigationView() {
+    clearAnimation()
+    visibility = View.VISIBLE
+    animate()
+        .translationY(0f)
+        .setDuration(100)
+        .start()
+}
--- android/app/src/main/java/com/daedan/festabook/presentation/placeList/PlaceListFragment.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/placeList/PlaceListFragment.kt
@@ -1,17 +1,22 @@
 package com.daedan.festabook.presentation.placeList
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.commit
 import androidx.fragment.app.viewModels
 import com.daedan.festabook.R
 import com.daedan.festabook.databinding.FragmentPlaceListBinding
 import com.daedan.festabook.presentation.common.BaseFragment
+import com.daedan.festabook.presentation.common.animateHideBottomNavigationView
+import com.daedan.festabook.presentation.common.animateShowBottomNavigationView
 import com.daedan.festabook.presentation.placeDetail.PlaceDetailFragment
 import com.daedan.festabook.presentation.placeList.dummy.DummyPlace
 import com.daedan.festabook.presentation.placeList.uimodel.Place
 import com.daedan.festabook.presentation.placeList.uimodel.PlaceListEvent
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class PlaceListFragment :
     BaseFragment<FragmentPlaceListBinding>(
@@ -48,9 +53,34 @@ class PlaceListFragment :
     }
 
     private fun startPlaceDetailFragment() {
+        parentFragmentManager.registerFragmentLifecycleCallbacks(
+            object : FragmentManager.FragmentLifecycleCallbacks() {
+                override fun onFragmentStopped(
+                    fm: FragmentManager,
+                    f: Fragment,
+                ) {
+                    activity?.findViewById<BottomNavigationView>(R.id.bnv_menu)?.animateShowBottomNavigationView()
+                    super.onFragmentStopped(fm, f)
+                }
+
+                override fun onFragmentAttached(
+                    fm: FragmentManager,
+                    f: Fragment,
+                    context: Context,
+                ) {
+                    activity?.findViewById<BottomNavigationView>(R.id.bnv_menu)?.animateHideBottomNavigationView()
+                    super.onFragmentAttached(fm, f, context)
+                }
+            },
+            false,
+        )
         parentFragmentManager.commit {
-            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-            setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE)
+            setCustomAnimations(
+                R.anim.anim_fade_in_left,
+                R.anim.anim_fade_out,
+                R.anim.anim_fade_in_right,
+                R.anim.anim_fade_out,
+            )
             add(
                 R.id.fcv_fragment_container,
                 PlaceDetailFragment.newInstance(
