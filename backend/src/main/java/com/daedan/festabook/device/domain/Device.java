c339fbac299a8fb6bfa659aad84e0d7a40da1fe3
soeun2537 <soeun2537@gmail.com>
2025-07-17 21:23:19 +0900

    refactor: Device 엔티티 필드 수정

--- backend/src/main/java/com/daedan/festabook/device/domain/Device.java
+++ backend/src/main/java/com/daedan/festabook/device/domain/Device.java
@@ -1,6 +1,5 @@
 package com.daedan.festabook.device.domain;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,17 +17,13 @@ public class Device {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String deviceId;
-
-    @Column(nullable = false)
     private String fcmToken;
 
+    // TODO: 마지막 접근한 시간 추후 구현
+
     public Device(
-            String deviceId,
             String fcmToken
     ) {
-        this.deviceId = deviceId;
         this.fcmToken = fcmToken;
     }
 }
