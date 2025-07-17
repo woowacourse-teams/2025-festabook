b8cec7549b0ac5e78b87eea994d7e0d3c634d053
soeun2537 <soeun2537@gmail.com>
2025-07-17 21:35:11 +0900

    feat: 공지 생성 로직에 알림 요청 기능 추가

--- backend/src/main/java/com/daedan/festabook/announcement/service/AnnouncementService.java
+++ backend/src/main/java/com/daedan/festabook/announcement/service/AnnouncementService.java
@@ -6,11 +6,15 @@ import com.daedan.festabook.announcement.dto.AnnouncementResponse;
 import com.daedan.festabook.announcement.dto.AnnouncementResponses;
 import com.daedan.festabook.announcement.infrastructure.AnnouncementJpaRepository;
 import com.daedan.festabook.global.exception.BusinessException;
+import com.daedan.festabook.notification.constants.TopicConstants;
+import com.daedan.festabook.notification.dto.NotificationRequest;
+import com.daedan.festabook.notification.service.NotificationService;
 import com.daedan.festabook.organization.domain.Organization;
 import com.daedan.festabook.organization.infrastructure.OrganizationJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,12 +22,20 @@ public class AnnouncementService {
 
     private final AnnouncementJpaRepository announcementJpaRepository;
     private final OrganizationJpaRepository organizationJpaRepository;
+    private final NotificationService notificationService;
 
+    @Transactional
     public AnnouncementResponse createAnnouncement(Long organizationId, AnnouncementRequest request) {
         Organization organization = getOrganizationById(organizationId);
-
         Announcement announcement = request.toEntity(organization);
         announcementJpaRepository.save(announcement);
+
+        NotificationRequest notificationRequest = new NotificationRequest(
+                TopicConstants.getOrganizationTopicById(organizationId),
+                request.title(),
+                request.content()
+        );
+        notificationService.sendToTopic(notificationRequest);
 
         return AnnouncementResponse.from(announcement);
     }
