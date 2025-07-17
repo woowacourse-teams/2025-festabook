8b43eb379b89ab2000b4cd60792e2e2d934fd98b
taek2222 <oot309@daum.net>
2025-07-17 15:42:30 +0900

    feat: Detail Repository 추가

---
+++ backend/src/main/java/com/daedan/festabook/place/infrastructure/PlaceDetailJpaRepository.java
@@ -0,0 +1,10 @@
+package com.daedan.festabook.place.infrastructure;
+
+import com.daedan.festabook.place.domain.Place;
+import com.daedan.festabook.place.domain.PlaceDetail;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlaceDetailJpaRepository extends JpaRepository<PlaceDetail, Long> {
+    List<PlaceDetail> findAllByPlaceIn(List<Place> attr0);
+}
