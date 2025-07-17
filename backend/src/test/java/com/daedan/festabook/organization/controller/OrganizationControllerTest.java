4de29330d901f08894906d70e40adb60577a7318
changuii <rhljh7410@gmail.com>
2025-07-17 17:13:33 +0900

    test: 테스트를 모든 필드 검증으로 변경

--- backend/src/test/java/com/daedan/festabook/organization/controller/OrganizationControllerTest.java
+++ backend/src/test/java/com/daedan/festabook/organization/controller/OrganizationControllerTest.java
@@ -1,6 +1,5 @@
 package com.daedan.festabook.organization.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import com.daedan.festabook.organization.domain.Organization;
@@ -8,6 +7,11 @@ import com.daedan.festabook.organization.domain.OrganizationFixture;
 import com.daedan.festabook.organization.dto.OrganizationGeographyResponse;
 import com.daedan.festabook.organization.infrastructure.OrganizationJpaRepository;
 import io.restassured.RestAssured;
+import io.restassured.config.JsonConfig;
+import io.restassured.config.RestAssuredConfig;
+import io.restassured.path.json.config.JsonPathConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
@@ -31,6 +35,18 @@ class OrganizationControllerTest {
     @LocalServerPort
     private int port;
 
+    @BeforeAll
+    static void beforeAll() {
+        RestAssured.config = RestAssured.config()
+                .jsonConfig(JsonConfig.jsonConfig()
+                        .numberReturnType(JsonPathConfig.NumberReturnType.DOUBLE));
+    }
+
+    @AfterAll
+    static void afterAll() {
+        RestAssured.config = RestAssuredConfig.config();
+    }
+
     @BeforeEach
     void setUp() {
         RestAssured.port = port;
@@ -50,7 +66,7 @@ class OrganizationControllerTest {
             OrganizationGeographyResponse expected = OrganizationGeographyResponse.from(organization);
 
             // when & then
-            OrganizationGeographyResponse result = RestAssured
+            RestAssured
                     .given()
                     .header(ORGANIZATION_HEADER_NAME, organization.getId())
                     .when()
@@ -58,10 +74,23 @@ class OrganizationControllerTest {
                     .then()
                     .statusCode(HttpStatus.OK.value())
                     .body("size()", equalTo(expectedFieldSize))
-                    .extract()
-                    .as(OrganizationGeographyResponse.class);
-
-            assertThat(result).isEqualTo(expected);
+                    .body("zoom", equalTo(organization.getZoom()))
+                    .body("centerCoordinate.latitude",
+                            equalTo(organization.getCenterCoordinate().getLatitude()))
+                    .body("centerCoordinate.longitude",
+                            equalTo(organization.getCenterCoordinate().getLongitude()))
+                    .body("polygonHoleBoundary[0].latitude",
+                            equalTo(organization.getPolygonHoleBoundary().get(0).getLatitude()))
+                    .body("polygonHoleBoundary[0].longitude",
+                            equalTo(organization.getPolygonHoleBoundary().get(0).getLongitude()))
+                    .body("polygonHoleBoundary[1].latitude",
+                            equalTo(organization.getPolygonHoleBoundary().get(1).getLatitude()))
+                    .body("polygonHoleBoundary[1].longitude",
+                            equalTo(organization.getPolygonHoleBoundary().get(1).getLongitude()))
+                    .body("polygonHoleBoundary[2].latitude",
+                            equalTo(organization.getPolygonHoleBoundary().get(2).getLatitude()))
+                    .body("polygonHoleBoundary[2].longitude",
+                            equalTo(organization.getPolygonHoleBoundary().get(2).getLongitude()));
         }
     }
 }
