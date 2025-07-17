55ec0518cd482177c131a5552112c4a2b9312383
eoehd1ek <eoehd2ek@gmail.com>
2025-07-17 16:51:49 +0900

    feat: OrganizationBookmark 엔티티 추가

---
+++ backend/src/main/java/com/daedan/festabook/organization/domain/OrganizationBookmark.java
@@ -0,0 +1,39 @@
+package com.daedan.festabook.organization.domain;
+
+import com.daedan.festabook.device.domain.Device;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class OrganizationBookmark {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Organization organization;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Device device;
+
+    public OrganizationBookmark(
+            Organization organization,
+            Device device
+    ) {
+        this.organization = organization;
+        this.device = device;
+    }
+}
