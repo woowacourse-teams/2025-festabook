ae509b124a56ad3020f9895bf6170f2f2a49fcb0
taek2222 <oot309@daum.net>
2025-07-20 15:05:43 +0900

    refactor: PlacePreview 서비스 분리

--- backend/src/main/java/com/daedan/festabook/place/service/PlaceService.java
+++ backend/src/main/java/com/daedan/festabook/place/service/PlaceService.java
@@ -5,16 +5,12 @@ import com.daedan.festabook.place.domain.Place;
 import com.daedan.festabook.place.domain.PlaceAnnouncement;
 import com.daedan.festabook.place.domain.PlaceDetail;
 import com.daedan.festabook.place.domain.PlaceImage;
-import com.daedan.festabook.place.dto.PlacePreviewResponses;
 import com.daedan.festabook.place.dto.PlaceResponse;
 import com.daedan.festabook.place.infrastructure.PlaceAnnouncementJpaRepository;
 import com.daedan.festabook.place.infrastructure.PlaceDetailJpaRepository;
 import com.daedan.festabook.place.infrastructure.PlaceImageJpaRepository;
 import com.daedan.festabook.place.infrastructure.PlaceJpaRepository;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -23,21 +19,10 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PlaceService {
 
-    private static final int REPRESENTATIVE_IMAGE_SEQUENCE = 1;
-
     private final PlaceJpaRepository placeJpaRepository;
     private final PlaceImageJpaRepository placeImageJpaRepository;
     private final PlaceDetailJpaRepository placeDetailJpaRepository;
     private final PlaceAnnouncementJpaRepository placeAnnouncementJpaRepository;
-
-    public PlacePreviewResponses getAllPreviewPlaceByOrganizationId(Long organizationId) {
-        List<Place> places = placeJpaRepository.findAllByOrganizationId(organizationId);
-
-        Map<Long, PlaceImage> placeImages = mapPlaceImagesToIds(places);
-        Map<Long, PlaceDetail> placeDetails = mapPlaceDetailsToIds(places);
-
-        return PlacePreviewResponses.from(places, placeDetails, placeImages);
-    }
 
     public PlaceResponse getPlaceByPlaceId(Long placeId) {
         Place place = getPlaceById(placeId);
@@ -48,31 +33,15 @@ public class PlaceService {
         return PlaceResponse.from(place, placeDetail, placeImages, placeAnnouncements);
     }
 
-    private Map<Long, PlaceImage> mapPlaceImagesToIds(List<Place> places) {
-        return placeImageJpaRepository.findAllByPlaceInAndSequence(places, REPRESENTATIVE_IMAGE_SEQUENCE).stream()
-                .collect(Collectors.toMap(
-                        image -> image.getPlace().getId(), // TODO: N+1 문제 해결
-                        Function.identity()
-                ));
-    }
-
-    private Map<Long, PlaceDetail> mapPlaceDetailsToIds(List<Place> places) {
-        return placeDetailJpaRepository.findAllByPlaceIn(places).stream()
-                .collect(Collectors.toMap(
-                        detail -> detail.getPlace().getId(), // TODO: N+1 문제 해결
-                        Function.identity()
-                ));
+    // TODO: ExceptionHandler 등록 후 예외 변경
+    private Place getPlaceById(Long placeId) {
+        return placeJpaRepository.findById(placeId)
+                .orElseThrow(() -> new BusinessException("존재하지 않는 플레이스입니다.", HttpStatus.NOT_FOUND));
     }
 
     // TODO: ExceptionHandler 등록 후 예외 변경
     private PlaceDetail getPlaceDetailById(Long placeId) {
         return placeDetailJpaRepository.findById(placeId)
                 .orElseThrow(() -> new BusinessException("존재하지 않는 플레이스 세부 정보입니다.", HttpStatus.NOT_FOUND));
-    }
-
-    // TODO: ExceptionHandler 등록 후 예외 변경
-    private Place getPlaceById(Long placeId) {
-        return placeJpaRepository.findById(placeId)
-                .orElseThrow(() -> new BusinessException("존재하지 않는 플레이스입니다.", HttpStatus.NOT_FOUND));
     }
 }
