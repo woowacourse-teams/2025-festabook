8ee78931d1361a0dbad5b1b2b531077ab59744e5
taek2222 <oot309@daum.net>
2025-07-20 16:16:55 +0900

    refactor: PlaceDetail 우선 조회로 에러 터지도록 변경

--- backend/src/main/java/com/daedan/festabook/place/service/PlaceService.java
+++ backend/src/main/java/com/daedan/festabook/place/service/PlaceService.java
@@ -1,7 +1,6 @@
 package com.daedan.festabook.place.service;
 
 import com.daedan.festabook.global.exception.BusinessException;
-import com.daedan.festabook.place.domain.Place;
 import com.daedan.festabook.place.domain.PlaceAnnouncement;
 import com.daedan.festabook.place.domain.PlaceDetail;
 import com.daedan.festabook.place.domain.PlaceImage;
@@ -9,34 +8,27 @@ import com.daedan.festabook.place.dto.PlaceResponse;
 import com.daedan.festabook.place.infrastructure.PlaceAnnouncementJpaRepository;
 import com.daedan.festabook.place.infrastructure.PlaceDetailJpaRepository;
 import com.daedan.festabook.place.infrastructure.PlaceImageJpaRepository;
-import com.daedan.festabook.place.infrastructure.PlaceJpaRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class PlaceService {
 
-    private final PlaceJpaRepository placeJpaRepository;
     private final PlaceImageJpaRepository placeImageJpaRepository;
     private final PlaceDetailJpaRepository placeDetailJpaRepository;
     private final PlaceAnnouncementJpaRepository placeAnnouncementJpaRepository;
 
+    @Transactional(readOnly = true)
     public PlaceResponse getPlaceByPlaceId(Long placeId) {
-        Place place = getPlaceById(placeId);
         PlaceDetail placeDetail = getPlaceDetailById(placeId);
         List<PlaceImage> placeImages = placeImageJpaRepository.findAllByPlaceIdOrderBySequenceAsc(placeId);
         List<PlaceAnnouncement> placeAnnouncements = placeAnnouncementJpaRepository.findAllByPlaceId(placeId);
 
-        return PlaceResponse.from(place, placeDetail, placeImages, placeAnnouncements);
-    }
-
-    // TODO: ExceptionHandler 등록 후 예외 변경
-    private Place getPlaceById(Long placeId) {
-        return placeJpaRepository.findById(placeId)
-                .orElseThrow(() -> new BusinessException("존재하지 않는 플레이스입니다.", HttpStatus.NOT_FOUND));
+        return PlaceResponse.from(placeDetail.getPlace(), placeDetail, placeImages, placeAnnouncements);
     }
 
     // TODO: ExceptionHandler 등록 후 예외 변경
