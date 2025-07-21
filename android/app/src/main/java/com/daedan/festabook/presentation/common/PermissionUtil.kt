b263e26de2c240d38fa4d197a127cdbdc4c1d5af
oungsi2000 <oungsi1000@gmail.com>
2025-07-21 19:58:07 +0900

    refactor: 위치 권한 거부 시 메시지 처리 함수 네이밍 수정

--- android/app/src/main/java/com/daedan/festabook/presentation/common/PermissionUtil.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/common/PermissionUtil.kt
@@ -16,7 +16,7 @@ fun Context.showToast(text: String) {
 
 fun Int.isGranted(): Boolean = this == 0
 
-fun Context.toLocationPermissionDeniedText(rawText: String) =
+fun Context.toLocationPermissionDeniedTextOrNull(rawText: String) =
     when (rawText) {
         Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION -> {
             getString(R.string.map_request_location_permission_message)
