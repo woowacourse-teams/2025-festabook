ff649cda9e6afadba06009a7fd63a4b60a68192a
taek2222 <oot309@daum.net>
2025-07-22 15:30:53 +0900

    test: 이벤트 기반 컨트롤러 테스트 추가

--- backend/src/test/java/com/daedan/festabook/schedule/controller/ScheduleControllerTest.java
+++ backend/src/test/java/com/daedan/festabook/schedule/controller/ScheduleControllerTest.java
@@ -3,6 +3,7 @@ package com.daedan.festabook.schedule.controller;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.BDDMockito.given;
 
 import com.daedan.festabook.organization.domain.Organization;
 import com.daedan.festabook.organization.domain.OrganizationFixture;
@@ -11,22 +12,29 @@ import com.daedan.festabook.schedule.domain.Event;
 import com.daedan.festabook.schedule.domain.EventDate;
 import com.daedan.festabook.schedule.domain.EventDateFixture;
 import com.daedan.festabook.schedule.domain.EventFixture;
+import com.daedan.festabook.schedule.domain.EventStatus;
 import com.daedan.festabook.schedule.infrastructure.EventDateJpaRepository;
 import com.daedan.festabook.schedule.infrastructure.EventJpaRepository;
 import io.restassured.RestAssured;
+import java.time.Clock;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -42,6 +50,9 @@ class ScheduleControllerTest {
 
     @Autowired
     private EventJpaRepository eventJpaRepository;
+
+    @MockitoBean
+    private Clock clock;
 
     @LocalServerPort
     private int port;
@@ -146,10 +157,17 @@ class ScheduleControllerTest {
             Organization organization = OrganizationFixture.create();
             organizationJpaRepository.save(organization);
 
-            EventDate eventDate = EventDateFixture.create(organization);
+            LocalDateTime dateTime = LocalDateTime.of(2025, 5, 20, 15, 0);
+            setFixedClock(dateTime);
+
+            EventDate eventDate = EventDateFixture.create(organization, dateTime.toLocalDate());
             eventDateJpaRepository.save(eventDate);
 
-            Event event = EventFixture.create(eventDate);
+            Event event = EventFixture.create(
+                    dateTime.toLocalTime().minusHours(2), // 시작 시간, 13:00
+                    dateTime.toLocalTime().plusHours(2), // 종료 시간, 17:00
+                    eventDate
+            );
             eventJpaRepository.save(event);
 
             int expectedSize = 1;
@@ -165,7 +183,7 @@ class ScheduleControllerTest {
                     .body("$", hasSize(expectedSize))
                     .body("[0].size()", equalTo(expectedFieldSize))
                     .body("[0].id", equalTo(event.getId().intValue()))
-                    .body("[0].status", equalTo(event.getStatus().name()))
+                    .body("[0].status", equalTo(EventStatus.ONGOING.name()))
                     .body("[0].startTime", equalTo(event.getStartTime().toString()))
                     .body("[0].endTime", equalTo(event.getEndTime().toString()))
                     .body("[0].title", equalTo(event.getTitle()))
@@ -181,6 +199,8 @@ class ScheduleControllerTest {
             EventDate eventDate = EventDateFixture.create(organization, LocalDate.now());
             EventDate otherEventDate = EventDateFixture.create(organization, LocalDate.now().plusDays(1));
             eventDateJpaRepository.saveAll(List.of(eventDate, otherEventDate));
+
+            setFixedClock(LocalDateTime.now());
 
             int expectedSize = 3;
             List<Event> events = EventFixture.createList(expectedSize, eventDate);
@@ -207,6 +227,8 @@ class ScheduleControllerTest {
             EventDate eventDate = EventDateFixture.create(organization);
             eventDateJpaRepository.save(eventDate);
 
+            setFixedClock(LocalDateTime.now());
+
             List<Event> events = List.of(
                     EventFixture.create(LocalTime.of(15, 0), LocalTime.of(18, 0), eventDate),
                     EventFixture.create(LocalTime.of(12, 0), LocalTime.of(15, 0), eventDate),
@@ -227,5 +249,51 @@ class ScheduleControllerTest {
                     .body("startTime", equalTo(expectedStartTime))
                     .body("endTime", equalTo(expectedEndTime));
         }
+
+        @ParameterizedTest(name = "날짜: {0}, 시작 시간: {1}, 종료 시간: {2}, 결과: {3}")
+        @CsvSource({
+                "2025-05-04, 10:00, 12:00, COMPLETED",   // 종료
+                "2025-05-05, 14:00, 17:00, ONGOING",     // 진행중
+                "2025-05-06, 10:00, 12:00, UPCOMING"     // 예정
+        })
+        void 성공_이벤트_상태_기반(LocalDate date, LocalTime startTime, LocalTime endTime, EventStatus status) {
+            // given
+            Organization organization = OrganizationFixture.create();
+            organizationJpaRepository.save(organization);
+
+            LocalDateTime dateTime = LocalDateTime.of(2025, 5, 5, 15, 0);
+            setFixedClock(dateTime);
+
+            EventDate eventDate = EventDateFixture.create(organization, date);
+            eventDateJpaRepository.save(eventDate);
+
+            Event event = EventFixture.create(
+                    startTime,
+                    endTime,
+                    eventDate
+            );
+            eventJpaRepository.save(event);
+
+            // when & then
+            RestAssured
+                    .given()
+                    .when()
+                    .get("/schedules/{eventDateId}", eventDate.getId())
+                    .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .body("[0].status", equalTo(status.name()));
+        }
+    }
+
+    private void setFixedClock(LocalDateTime dateTime) {
+        Clock fixedClock = Clock.fixed(
+                dateTime.atZone(ZoneId.systemDefault()).toInstant(),
+                ZoneId.systemDefault()
+        );
+
+        given(clock.instant())
+                .willReturn(fixedClock.instant());
+        given(clock.getZone())
+                .willReturn(fixedClock.getZone());
     }
 }
