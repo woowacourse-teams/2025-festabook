5658a424317d868ae42c0ddd364c691176ef083a
eoehd1ek <eoehd2ek@gmail.com>
2025-07-22 15:27:25 +0900

    refactor: FirebaseApp 로그인 모킹으로 대체

--- backend/src/main/java/com/daedan/festabook/global/config/FirebaseAppConfig.java
+++ backend/src/main/java/com/daedan/festabook/global/config/FirebaseAppConfig.java
@@ -4,40 +4,41 @@ import com.daedan.festabook.global.exception.BusinessException;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.messaging.FirebaseMessaging;
 import java.io.IOException;
 import java.io.InputStream;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpStatus;
 
 @Configuration
-public class FirebaseConfig {
+@Profile("!test")
+@RequiredArgsConstructor
+public class FirebaseAppConfig {
+
+    private final ResourceLoader resourceLoader;
 
     @Value("${firebase.adminsdk.account.path}")
     private String firebaseAccountPath;
 
     @Bean
     public FirebaseApp firebaseApp() {
-        try (InputStream serviceAccount = new ClassPathResource(firebaseAccountPath).getInputStream()) {
+        Resource resource = resourceLoader.getResource(firebaseAccountPath);
+        try (InputStream serviceAccount = resource.getInputStream()) {
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
                 return FirebaseApp.initializeApp(options);
-            } else {
-                return FirebaseApp.getInstance();
             }
+            return FirebaseApp.getInstance();
         } catch (IOException e) {
-            throw new BusinessException("Firebase 초기화에 실패했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new BusinessException("Firebase 초기화에 실패했습니다." + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
-    }
-
-    @Bean
-    public FirebaseMessaging firebaseMessaging(FirebaseApp firebaseApp) {
-        return FirebaseMessaging.getInstance(firebaseApp);
     }
 }
