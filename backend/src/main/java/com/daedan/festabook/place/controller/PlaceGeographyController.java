7664d93c586138c726b7f67dde4b23295da02480
taek2222 <oot309@daum.net>
2025-07-22 20:37:45 +0900

    feat: 플레이스 지리 정보 반환 컨트롤러 작성

---
+++ backend/src/main/java/com/daedan/festabook/place/controller/PlaceGeographyController.java
@@ -0,0 +1,37 @@
+package com.daedan.festabook.place.controller;
+
+import com.daedan.festabook.global.argumentresolver.OrganizationId;
+import com.daedan.festabook.place.dto.PlaceGeographyResponses;
+import com.daedan.festabook.place.service.PlaceGeographyService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/places/geographies")
+@Tag(name = "플레이스 지리", description = "플레이스 지리 관련 API")
+public class PlaceGeographyController {
+
+    private final PlaceGeographyService placeGeographyService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "특정 조직의 플레이스 모든 지리 정보 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", useReturnTypeSchema = true),
+    })
+    public PlaceGeographyResponses getAllPlaceGeographyByOrganizationId(
+            @Parameter(hidden = true) @OrganizationId Long organizationId
+    ) {
+        return placeGeographyService.getAllPlaceGeographyByOrganizationId(organizationId);
+    }
+}
