1fd6a520ed92dfaac3b43fb87582b9e70ed87427
soeun2537 <soeun2537@gmail.com>
2025-07-22 18:24:03 +0900

    feat: 조직 북마크 이미 존재하는 경우 검증 로직 추가

--- backend/src/main/java/com/daedan/festabook/organization/infrastructure/OrganizationBookmarkJpaRepository.java
+++ backend/src/main/java/com/daedan/festabook/organization/infrastructure/OrganizationBookmarkJpaRepository.java
@@ -4,4 +4,6 @@ import com.daedan.festabook.organization.domain.OrganizationBookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrganizationBookmarkJpaRepository extends JpaRepository<OrganizationBookmark, Long> {
+
+    boolean existsByOrganizationIdAndDeviceId(Long organizationId, Long deviceId);
 }
