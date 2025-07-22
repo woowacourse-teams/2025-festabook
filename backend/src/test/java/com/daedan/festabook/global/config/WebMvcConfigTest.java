6187435464df60b66bcce972f72ce2271d980e24
changuii <rhljh7410@gmail.com>
2025-07-22 21:44:56 +0900

    chore: CORS 설정 추가

--- backend/src/test/java/com/daedan/festabook/global/config/WebMvcConfigTest.java
+++ backend/src/test/java/com/daedan/festabook/global/config/WebMvcConfigTest.java
@@ -59,6 +59,44 @@ class WebMvcConfigTest {
         }
     }
 
+    @Nested
+    class addCorsMappings {
+
+        @Test
+        void 성공() {
+            // given
+            String origin1 = "http://localhost:5173";
+            String origin2 = "http://127.0.0.1:5173";
+            String expectedResponse = "addCorsMappings";
+            String expectedAllowCredentials = "true";
+
+            // when & then
+            RestAssured.
+                    given()
+                    .header("Origin", origin1)
+                    .when()
+                    .get("/test/addCorsMappings")
+                    .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .log().all()
+                    .body(equalTo(expectedResponse))
+                    .header("Access-Control-Allow-Origin", equalTo(origin1))
+                    .header("Access-Control-Allow-Credentials", equalTo(expectedAllowCredentials));
+
+            RestAssured.
+                    given()
+                    .header("Origin", origin2)
+                    .when()
+                    .get("/test/addCorsMappings")
+                    .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .log().all()
+                    .body(equalTo(expectedResponse))
+                    .header("Access-Control-Allow-Origin", equalTo(origin2))
+                    .header("Access-Control-Allow-Credentials", equalTo(expectedAllowCredentials));
+        }
+    }
+
     @RestController
     static class TestController {
 
@@ -66,6 +104,12 @@ class WebMvcConfigTest {
         @ResponseStatus(HttpStatus.OK)
         public Long test(@OrganizationId Long organizationId) {
             return organizationId;
+        }
+
+        @GetMapping("/test/addCorsMappings")
+        @ResponseStatus(HttpStatus.OK)
+        public String test() {
+            return "addCorsMappings";
         }
     }
 }
