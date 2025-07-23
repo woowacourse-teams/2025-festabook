def431b5e4c13cb5f01eb911eeaf98fa8df91fb6
taek2222 <oot309@daum.net>
2025-07-23 19:44:50 +0900

    test: Announcement Fixture 추가 생성

--- backend/src/test/java/com/daedan/festabook/announcement/domain/AnnouncementFixture.java
+++ backend/src/test/java/com/daedan/festabook/announcement/domain/AnnouncementFixture.java
@@ -2,19 +2,28 @@ package com.daedan.festabook.announcement.domain;
 
 import com.daedan.festabook.organization.domain.Organization;
 import com.daedan.festabook.organization.domain.OrganizationFixture;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class AnnouncementFixture {
 
-    private static final Organization DEFAULT_ORGANIZATION = OrganizationFixture.create();
     private static final String DEFAULT_TITLE = "폭우가 내립니다.";
     private static final String DEFAULT_CONTENT = "우산을 챙겨주세요.";
     private static final boolean DEFAULT_IS_PINNED = false;
+    private static final Organization DEFAULT_ORGANIZATION = OrganizationFixture.create();
+    private static final LocalDateTime DEFAULT_CREATED_AT = LocalDateTime.now();
 
     public static Announcement create() {
         return new Announcement(DEFAULT_TITLE, DEFAULT_CONTENT, DEFAULT_IS_PINNED, DEFAULT_ORGANIZATION);
+    }
+
+    public static Announcement create(
+            boolean isPinned
+    ) {
+        return new Announcement(null, DEFAULT_TITLE, DEFAULT_CONTENT, isPinned, DEFAULT_ORGANIZATION,
+                DEFAULT_CREATED_AT);
     }
 
     public static Announcement create(
@@ -23,12 +32,36 @@ public class AnnouncementFixture {
         return new Announcement(DEFAULT_TITLE, DEFAULT_CONTENT, DEFAULT_IS_PINNED, organization);
     }
 
+    public static Announcement create(
+            boolean isPinned,
+            LocalDateTime createdAt
+    ) {
+        return new Announcement(null, DEFAULT_TITLE, DEFAULT_CONTENT, isPinned, DEFAULT_ORGANIZATION, createdAt);
+    }
+
+    public static Announcement create(
+            boolean isPinned,
+            Organization organization
+    ) {
+        return new Announcement(DEFAULT_TITLE, DEFAULT_CONTENT, isPinned, organization);
+    }
+
     public static List<Announcement> createList(
             int size,
             Organization organization
     ) {
         return IntStream.range(0, size)
                 .mapToObj(i -> create(organization))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Announcement> createList(
+            int size,
+            boolean isPinned,
+            Organization organization
+    ) {
+        return IntStream.range(0, size)
+                .mapToObj(i -> create(isPinned, organization))
                 .collect(Collectors.toList());
     }
 }
