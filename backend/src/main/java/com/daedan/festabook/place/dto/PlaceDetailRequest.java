3c7e11164b9ed312f058e2342cf51e74fd183409
changuii <rhljh7410@gmail.com>
2025-07-24 22:53:02 +0900

    fix: 커밋에서 제외된 파일 추가

---
+++ backend/src/main/java/com/daedan/festabook/place/dto/PlaceDetailRequest.java
@@ -0,0 +1,24 @@
+package com.daedan.festabook.place.dto;
+
+import com.daedan.festabook.organization.domain.Organization;
+import com.daedan.festabook.place.domain.Place;
+import com.daedan.festabook.place.domain.PlaceCategory;
+import com.daedan.festabook.place.domain.PlaceDetail;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record PlaceDetailRequest(
+
+        @Schema(description = "플레이스 카테고리", example = "BAR")
+        PlaceCategory category,
+
+        @Schema(description = "플레이스 이름", example = "웃지않는 미소 동아리")
+        String title
+) {
+
+    public PlaceDetail toPlaceDetail(Organization organization) {
+        return new PlaceDetail(
+                new Place(organization, category),
+                title
+        );
+    }
+}
