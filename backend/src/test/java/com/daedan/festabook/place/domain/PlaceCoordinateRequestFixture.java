9e058f25cc9d33f81b17f1cf8a332a8f1977e759
soeun2537 <soeun2537@gmail.com>
2025-07-24 20:25:04 +0900

    refactor: 미사용 메서드 제거

--- backend/src/test/java/com/daedan/festabook/place/domain/PlaceCoordinateRequestFixture.java
+++ backend/src/test/java/com/daedan/festabook/place/domain/PlaceCoordinateRequestFixture.java
@@ -7,16 +7,6 @@ public class PlaceCoordinateRequestFixture {
     private static final Double DEFAULT_LATITUDE = 37.1;
     private static final Double DEFAULT_LONGITUDE = 127.1;
 
-    public static PlaceCoordinateRequest create(
-            Double latitude,
-            Double longitude
-    ) {
-        return new PlaceCoordinateRequest(
-                latitude,
-                longitude
-        );
-    }
-
     public static PlaceCoordinateRequest create() {
         return new PlaceCoordinateRequest(
                 DEFAULT_LATITUDE,
