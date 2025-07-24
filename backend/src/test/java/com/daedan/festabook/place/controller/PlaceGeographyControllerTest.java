4df5588b0d432b15a0639e569fc123a7867f17b3
soeun2537 <soeun2537@gmail.com>
2025-07-24 13:30:40 +0900

    test: 지도 좌표 설정 API 테스트 작성

--- backend/src/test/java/com/daedan/festabook/place/controller/PlaceGeographyControllerTest.java
+++ backend/src/test/java/com/daedan/festabook/place/controller/PlaceGeographyControllerTest.java
@@ -1,17 +1,21 @@
 package com.daedan.festabook.place.controller;
 
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.hasSize;
 
 import com.daedan.festabook.organization.domain.Organization;
 import com.daedan.festabook.organization.domain.OrganizationFixture;
 import com.daedan.festabook.organization.infrastructure.OrganizationJpaRepository;
 import com.daedan.festabook.place.domain.Place;
+import com.daedan.festabook.place.domain.PlaceCoordinateRequestFixture;
 import com.daedan.festabook.place.domain.PlaceFixture;
+import com.daedan.festabook.place.dto.PlaceCoordinateRequest;
 import com.daedan.festabook.place.infrastructure.PlaceJpaRepository;
 import io.restassured.RestAssured;
 import io.restassured.config.JsonConfig;
 import io.restassured.config.RestAssuredConfig;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.config.JsonPathConfig;
 import java.util.List;
 import org.junit.jupiter.api.AfterAll;
@@ -111,6 +115,59 @@ class PlaceGeographyControllerTest {
                     .then()
                     .statusCode(HttpStatus.OK.value())
                     .body("$", hasSize(expectedSize));
+        }
+    }
+
+    @Nested
+    class updatePlaceCoordinate {
+
+        @Test
+        void 성공() {
+            // given
+            Organization organization = OrganizationFixture.create();
+            organizationJpaRepository.save(organization);
+
+            Place place = PlaceFixture.create(organization);
+            placeJpaRepository.save(place);
+
+            PlaceCoordinateRequest request = PlaceCoordinateRequestFixture.create();
+
+            int expectedFieldSize = 2;
+            int expectedCoordinateSize = 2;
+
+            // when & then
+            RestAssured
+                    .given()
+                    .contentType(ContentType.JSON)
+                    .body(request)
+                    .when()
+                    .patch("/places/{placeId}/geographies", place.getId())
+                    .then()
+                    .statusCode(HttpStatus.OK.value())
+                    .body("size()", equalTo(expectedFieldSize))
+                    .body("$", hasKey("markerCoordinate"))
+                    .body("markerCoordinate.size()", equalTo(expectedCoordinateSize))
+                    .body("id", equalTo(place.getId().intValue()))
+                    .body("markerCoordinate.latitude", equalTo(request.latitude()))
+                    .body("markerCoordinate.longitude", equalTo(request.longitude()));
+        }
+
+        @Test
+        void 예외_존재하지_않는_플레이스() {
+            // given
+            Long invalidPlaceId = 0L;
+            PlaceCoordinateRequest request = PlaceCoordinateRequestFixture.create();
+
+            // when & then
+            RestAssured
+                    .given()
+                    .contentType(ContentType.JSON)
+                    .body(request)
+                    .when()
+                    .patch("/places/{placeId}/geographies", invalidPlaceId)
+                    .then()
+                    .statusCode(HttpStatus.NOT_FOUND.value())
+                    .body("message", equalTo("존재하지 않는 플레이스입니다."));
         }
     }
 }
--- backend/src/test/java/com/daedan/festabook/place/service/PlaceGeographyServiceTest.java
+++ backend/src/test/java/com/daedan/festabook/place/service/PlaceGeographyServiceTest.java
@@ -1,16 +1,22 @@
 package com.daedan.festabook.place.service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.mockito.BDDMockito.given;
 
+import com.daedan.festabook.global.exception.BusinessException;
 import com.daedan.festabook.organization.domain.Organization;
 import com.daedan.festabook.organization.domain.OrganizationFixture;
 import com.daedan.festabook.place.domain.Place;
 import com.daedan.festabook.place.domain.PlaceCategory;
+import com.daedan.festabook.place.domain.PlaceCoordinateRequestFixture;
 import com.daedan.festabook.place.domain.PlaceFixture;
+import com.daedan.festabook.place.dto.PlaceCoordinateRequest;
+import com.daedan.festabook.place.dto.PlaceCoordinateResponse;
 import com.daedan.festabook.place.dto.PlaceGeographyResponses;
 import com.daedan.festabook.place.infrastructure.PlaceJpaRepository;
 import java.util.List;
+import java.util.Optional;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
@@ -65,6 +71,47 @@ class PlaceGeographyServiceTest {
                 s.assertThat(result.responses().getLast().markerCoordinate().longitude())
                         .isEqualTo(place2.getCoordinate().getLongitude());
             });
+        }
+
+        @Nested
+        class updatePlaceCoordinate {
+
+            @Test
+            void 성공() {
+                // given
+                Long placeId = 1L;
+                Organization organization = OrganizationFixture.create(1L);
+                Place place = PlaceFixture.create(placeId, organization, null, null);
+                PlaceCoordinateRequest request = PlaceCoordinateRequestFixture.create();
+
+                given(placeJpaRepository.findById(placeId))
+                        .willReturn(Optional.of(place));
+
+                // when
+                PlaceCoordinateResponse result = placeGeographyService.updatePlaceCoordinate(placeId, request);
+
+                // then
+                assertSoftly(s -> {
+                    s.assertThat(result.id()).isEqualTo(placeId);
+                    s.assertThat(result.markerCoordinate().latitude()).isEqualTo(request.latitude());
+                    s.assertThat(result.markerCoordinate().longitude()).isEqualTo(request.longitude());
+                });
+            }
+
+            @Test
+            void 예외_존재하지_않는_플레이스() {
+                // given
+                Long placeId = 1L;
+                PlaceCoordinateRequest request = PlaceCoordinateRequestFixture.create();
+
+                given(placeJpaRepository.findById(placeId))
+                        .willReturn(Optional.empty());
+
+                // when & then
+                assertThatThrownBy(() -> placeGeographyService.updatePlaceCoordinate(placeId, request))
+                        .isInstanceOf(BusinessException.class)
+                        .hasMessage("존재하지 않는 플레이스입니다.");
+            }
         }
     }
 }
