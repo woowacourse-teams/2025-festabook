0acfb358f342689fc39dc7f3df2815f7f3f530c3
DongjooSEO <readyjoo24@gmail.com>
2025-07-24 13:29:23 +0900

    refactor: FirebaseMessagingService 로그 Timber로 변경

--- android/app/src/main/java/com/daedan/festabook/service/MyFirebaseMessagingService.kt
+++ android/app/src/main/java/com/daedan/festabook/service/MyFirebaseMessagingService.kt
@@ -1,29 +1,29 @@
 package com.daedan.festabook.service
 
-import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import timber.log.Timber
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     override fun onNewToken(token: String) {
-        Log.d("FCM_Token", "Refreshed token: $token")
+        Timber.d("Refreshed token: $token")
     }
 
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        Log.d("FCM_MESSAGE", "From: ${remoteMessage.from}")
+        Timber.d("From: ${remoteMessage.from}")
 
         // remoteMessage.notification
         remoteMessage.notification?.let { notification ->
             val title = notification.title
             val body = notification.body
-            Log.d("FCM_MESSAGE", "Notification Title: $title, Body: $body")
+            Timber.d("Notification Title: $title, Body: $body")
 
             NotificationHelper.showNotification(this, title, body)
         }
 
         // remoteMessage.data
         if (remoteMessage.data.isNotEmpty()) {
-            Log.d("FCM_MESSAGE", "Data Payload: ${remoteMessage.data}")
+            Timber.d("Data Payload: ${remoteMessage.data}")
             val targetId = remoteMessage.data["targetId"]
             val customTitle = remoteMessage.data["custom_title"]
             val customMessage = remoteMessage.data["custom_message"]
