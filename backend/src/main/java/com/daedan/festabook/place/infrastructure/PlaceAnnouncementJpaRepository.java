b9437f36e6c7b084c4a238ec2e2d808a29e30aef
changuii <rhljh7410@gmail.com>
2025-07-27 02:56:37 +0900

    remove: 잘못된 메서드 제거

--- backend/src/main/java/com/daedan/festabook/place/infrastructure/PlaceAnnouncementJpaRepository.java
+++ backend/src/main/java/com/daedan/festabook/place/infrastructure/PlaceAnnouncementJpaRepository.java
@@ -1,6 +1,5 @@
 package com.daedan.festabook.place.infrastructure;
 
-import com.daedan.festabook.place.domain.Place;
 import com.daedan.festabook.place.domain.PlaceAnnouncement;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface PlaceAnnouncementJpaRepository extends JpaRepository<PlaceAnnouncement, Long> {
 
     List<PlaceAnnouncement> findAllByPlaceId(Long placeId);
-
-    Long place(Place place);
 }
