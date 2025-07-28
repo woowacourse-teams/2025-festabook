49cfe0f128f62bd23a7449bf4dfd286b69e96bac
eoehd1ek <eoehd2ek@gmail.com>
2025-07-28 20:52:22 +0900

    refactor: 줄바꿈 컨벤션 수정

--- backend/src/main/java/com/daedan/festabook/schedule/dto/EventDateRequest.java
+++ backend/src/main/java/com/daedan/festabook/schedule/dto/EventDateRequest.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDate;
 
 public record EventDateRequest(
+        
         @Schema(description = "일정 날짜", example = "2025-07-18")
         LocalDate date
 ) {
