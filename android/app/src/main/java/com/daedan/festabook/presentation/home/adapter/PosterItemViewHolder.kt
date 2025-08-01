0821a0dab6d98f34398e4e16bde41e035583ae49
DongjooSEO <readyjoo24@gmail.com>
2025-08-01 15:54:12 +0900

    refactor: 홈 화면 포스터 이미지 로딩 개선

--- android/app/src/main/java/com/daedan/festabook/presentation/home/adapter/PosterItemViewHolder.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/home/adapter/PosterItemViewHolder.kt
@@ -1,9 +1,16 @@
 package com.daedan.festabook.presentation.home.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toDrawable
 import androidx.recyclerview.widget.RecyclerView
 import coil3.load
+import coil3.request.CachePolicy
+import coil3.request.crossfade
+import coil3.request.placeholder
+import coil3.request.transformations
+import coil3.transform.RoundedCornersTransformation
 import com.daedan.festabook.R
 import com.daedan.festabook.databinding.ItemHomePosterBinding
 
@@ -11,7 +18,12 @@ class PosterItemViewHolder(
     val binding: ItemHomePosterBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(url: String) {
-        binding.ivHomePoster.load(url)
+        binding.ivHomePoster.load(url) {
+            crossfade(true)
+            placeholder(Color.LTGRAY.toDrawable())
+            transformations(RoundedCornersTransformation(20f))
+            memoryCachePolicy(CachePolicy.ENABLED)
+        }
         binding.motionLayout.progress = 0f
         binding.motionLayout.transitionToState(R.id.collapsed)
     }
