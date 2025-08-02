034ceb037c04dfbadece8c3a5518a156af24dd59
oungsi2000 <oungsi1000@gmail.com>
2025-08-02 18:27:28 +0900

    feat: 공지사항 개수 및 정렬 로직 구현

--- android/app/src/main/java/com/daedan/festabook/domain/model/PlaceDetail.kt
+++ android/app/src/main/java/com/daedan/festabook/domain/model/PlaceDetail.kt
@@ -5,9 +5,15 @@ import java.time.LocalTime
 data class PlaceDetail(
     val id: Long,
     val place: Place,
-    val notices: List<Notice>,
+    private val _notices: List<Notice>,
     val host: String?,
     val startTime: LocalTime?,
     val endTime: LocalTime?,
-    val images: List<PlaceDetailImage>,
-)
+    private val _images: List<PlaceDetailImage>,
+) {
+    val notices: List<Notice> =
+        _notices
+            .sortedByDescending { it.createdAt }
+            .take(3)
+    val images: List<PlaceDetailImage> = _images.sortedBy { it.sequence }
+}
