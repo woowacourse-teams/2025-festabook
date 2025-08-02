8bc8702b375dd6d1d2fd94a4802d1543cc755782
박지민 <pjm5489@naver.com>
2025-08-02 16:23:21 +0900

    feat: FAQ API 연동
    - FAQ 관련 Service, DataSource, Repository 추가
    - FAQ 데이터 모델(FAQResponse, FAQItem) 추가
    - FAQ API 호출을 위한 Retrofit 설정 추가

---
+++ android/app/src/main/java/com/daedan/festabook/data/repository/FAQRepositoryImpl.kt
@@ -0,0 +1,16 @@
+package com.daedan.festabook.data.repository
+
+import com.daedan.festabook.data.datasource.remote.faq.FAQDataSource
+import com.daedan.festabook.data.model.response.toDomain
+import com.daedan.festabook.data.util.toResult
+import com.daedan.festabook.domain.model.FAQItem
+import com.daedan.festabook.domain.repository.FAQRepository
+
+class FAQRepositoryImpl(
+    private val faqDataSource: FAQDataSource,
+) : FAQRepository {
+    override suspend fun getAllFAQ(): Result<List<FAQItem>> {
+        val response = faqDataSource.fetchAllFAQs().toResult()
+        return response.mapCatching { fAQItems -> fAQItems.map { it.toDomain() } }
+    }
+}
