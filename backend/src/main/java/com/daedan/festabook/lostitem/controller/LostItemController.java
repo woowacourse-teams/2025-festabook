f024c19718c9d4d833e9fef59603b3e0d81a3be3
taek2222 <oot309@daum.net>
2025-08-05 11:15:10 +0900

    feat: LostItem 컨트롤러 및 DTO 추가

---
+++ backend/src/main/java/com/daedan/festabook/lostitem/controller/LostItemController.java
@@ -0,0 +1,51 @@
+package com.daedan.festabook.lostitem.controller;
+
+import com.daedan.festabook.global.argumentresolver.OrganizationId;
+import com.daedan.festabook.lostitem.service.LostItemService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/lost-item")
+@Tag(name = "분실물", description = "분실물 관련 API")
+class LostItemController {
+
+    private final LostItemService lostItemService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "특정 조직의 분실물 생성")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", useReturnTypeSchema = true),
+    })
+    public LostItemResponse createLostItem(
+            @Parameter(hidden = true) @OrganizationId Long organizationId,
+            @RequestBody LostItemRequest request
+    ) {
+        return lostItemService.createLostItem(organizationId, request);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "특정 조직의 모든 분실물 조회")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", useReturnTypeSchema = true),
+    })
+    public LostItemResponses getAllLostItemByOrganizationId(
+            @Parameter(hidden = true) @OrganizationId Long organizationId
+    ) {
+        return lostItemService.getAllLostItemByOrganizationId(organizationId);
+    }
+}
---
+++ backend/src/main/java/com/daedan/festabook/lostitem/controller/LostItemRequest.java
@@ -0,0 +1,20 @@
+package com.daedan.festabook.lostitem.controller;
+
+import com.daedan.festabook.lostitem.domain.LostItem;
+import com.daedan.festabook.lostitem.domain.PickupStatus;
+import com.daedan.festabook.organization.domain.Organization;
+
+public record LostItemRequest(
+        String imageUrl,
+        String storageLocation
+) {
+
+    public LostItem toLostItem(Organization organization) {
+        return new LostItem(
+                organization,
+                imageUrl,
+                storageLocation,
+                PickupStatus.PENDING
+        );
+    }
+}
