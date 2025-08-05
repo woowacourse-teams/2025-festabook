ce2a1bd227213f9c34106b95a889c86ad541ec21
taek2222 <oot309@daum.net>
2025-08-05 13:14:04 +0900

    refactor: JsonValue 사용

--- backend/src/main/java/com/daedan/festabook/lostitem/dto/LostItemResponses.java
+++ backend/src/main/java/com/daedan/festabook/lostitem/dto/LostItemResponses.java
@@ -1,10 +1,11 @@
 package com.daedan.festabook.lostitem.dto;
 
 import com.daedan.festabook.lostitem.domain.LostItem;
+import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.List;
 
 public record LostItemResponses(
-        List<LostItemResponse> responses
+        @JsonValue List<LostItemResponse> responses
 ) {
 
     public static LostItemResponses from(List<LostItem> lostItems) {
