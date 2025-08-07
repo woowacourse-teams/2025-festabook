d8a70c5c03e83ea80ad36a095c77fa53dda77953
박지민 <pjm5489@naver.com>
2025-08-08 01:21:48 +0900

    refactor: RecyclerView를 ScrollView와 LinearLayout으로 변경

--- android/app/src/main/java/com/daedan/festabook/presentation/news/faq/FAQFragment.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/news/faq/FAQFragment.kt
@@ -2,29 +2,26 @@ package com.daedan.festabook.presentation.news.faq
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import com.daedan.festabook.R
 import com.daedan.festabook.databinding.FragmentFAQBinding
+import com.daedan.festabook.databinding.ItemFaqBinding
 import com.daedan.festabook.presentation.common.BaseFragment
 import com.daedan.festabook.presentation.common.showErrorSnackBar
+import com.daedan.festabook.presentation.common.toPx
 import com.daedan.festabook.presentation.news.NewsViewModel
-import com.daedan.festabook.presentation.news.faq.adapter.FAQAdapter
+import com.daedan.festabook.presentation.news.faq.model.FAQItemUiModel
 import com.daedan.festabook.presentation.news.notice.adapter.OnNewsClickListener
 
 class FAQFragment : BaseFragment<FragmentFAQBinding>(R.layout.fragment_f_a_q) {
     private val viewModel: NewsViewModel by viewModels({ requireParentFragment() }) { NewsViewModel.Factory }
-    private val adapter by lazy {
-        FAQAdapter(requireParentFragment() as OnNewsClickListener)
-    }
 
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
-        binding.rvFaq.adapter = adapter
-        binding.lifecycleOwner = viewLifecycleOwner
-        binding.rvFaq.itemAnimator = null
         setupObservers()
     }
 
@@ -34,7 +31,7 @@ class FAQFragment : BaseFragment<FragmentFAQBinding>(R.layout.fragment_f_a_q) {
                 FAQUiState.InitialLoading -> {}
                 FAQUiState.Refreshing -> {}
                 is FAQUiState.Success -> {
-                    adapter.submitList(state.faqs)
+                    setupScrollview(state.faqs)
                 }
 
                 is FAQUiState.Error -> {
@@ -44,7 +41,49 @@ class FAQFragment : BaseFragment<FragmentFAQBinding>(R.layout.fragment_f_a_q) {
         }
     }
 
+    private fun setupScrollview(items: List<FAQItemUiModel>) {
+        binding.llFaqContainer.removeAllViews()
+
+        items.forEachIndexed { index, item ->
+            val faqItemBinding =
+                ItemFaqBinding.inflate(layoutInflater, binding.llFaqContainer, false)
+
+            setupFAQItemTopMargin(faqItemBinding, index)
+            setupFAQItemView(faqItemBinding, item)
+
+            binding.llFaqContainer.addView(faqItemBinding.root)
+        }
+    }
+
+    private fun setupFAQItemView(
+        faqItemBinding: ItemFaqBinding,
+        item: FAQItemUiModel,
+    ) {
+        faqItemBinding.tvFaqQuestion.text =
+            getString(R.string.tab_faq_question, item.question)
+        faqItemBinding.tvFaqAnswer.text = item.answer
+        faqItemBinding.ivFaqExpand.setImageResource(
+            if (item.isExpanded) R.drawable.ic_chevron_up else R.drawable.ic_chevron_down,
+        )
+        faqItemBinding.tvFaqAnswer.visibility = if (item.isExpanded) View.VISIBLE else View.GONE
+
+        faqItemBinding.clFaqItem.setOnClickListener {
+            (requireParentFragment() as OnNewsClickListener).onFAQClick(item)
+        }
+    }
+
+    private fun setupFAQItemTopMargin(
+        faqItemBinding: ItemFaqBinding,
+        index: Int,
+    ) {
+        val layoutParams = faqItemBinding.root.layoutParams as ViewGroup.MarginLayoutParams
+        layoutParams.topMargin = if (index == 0) TOP_MARGIN.toPx(requireContext()) else 0
+        faqItemBinding.root.layoutParams = layoutParams
+    }
+
     companion object {
+        private const val TOP_MARGIN = 12
+
         fun newInstance() = FAQFragment()
     }
 }
