da3753b6fa46525eb4001c3127b89ed48875fcfd
eoehd1ek <eoehd2ek@gmail.com>
2025-08-07 21:28:37 +0900

    feat: S3Client 빈 등록 추가

---
+++ backend/src/main/java/com/daedan/festabook/global/config/S3ClientConfig.java
@@ -0,0 +1,25 @@
+package com.daedan.festabook.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+@Profile("prod")
+@RequiredArgsConstructor
+public class S3ClientConfig {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.of(region))
+                .build();
+    }
+}
