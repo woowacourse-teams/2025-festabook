ce024bb16a566a12f39ac76ae9937a35be48ad97
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-08-07 22:44:12 +0900

    [AN] 설정 화면에 서비스 이용약관 및 문의하기 기능 추가 (#411)
    * feat: 설정 화면에 서비스 이용약관 및 문의하기 기능 추가
    
    * test: NewsViewModel, PlaceDetailViewModel, PlaceListViewModel 테스트 수정
    
    - NewsViewModelTest에 LostItemRepository 의존성 추가 및 관련 테스트 수정
    - PlaceDetailViewModelTest에서 loadPlaceDetail 호출 시 placeId 전달하도록 수정
    - PlaceListViewModelTest에 PlaceDetailRepository 의존성 추가 및 관련 테스트 수정
    - HomeViewModel의 loadFestival 가시성 public으로 변경
    - NewsTestFixture에 FAKE_LOST_ITEM 추가
    
    * feat: 문자열 리소스 설정
    
    - `SettingFragment`에서 사용되는 문자열을 `strings.xml`로 이동
    - 서비스 정책 URL 및 문의하기 이메일 주소 문자열 리소스 추가

--- android/app/src/main/java/com/daedan/festabook/presentation/setting/SettingFragment.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/setting/SettingFragment.kt
@@ -1,7 +1,10 @@
 package com.daedan.festabook.presentation.setting
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.core.net.toUri
 import com.daedan.festabook.R
 import com.daedan.festabook.databinding.FragmentSettingBinding
 import com.daedan.festabook.presentation.common.BaseFragment
@@ -12,5 +15,28 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_s
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupServicePolicyClickListener()
+        setupContactUsButtonClickListener()
+    }
+
+    private fun setupServicePolicyClickListener() {
+        binding.ivSettingServicePolicy.setOnClickListener {
+            val url = getString(R.string.setting_service_policy_url)
+            val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+            startActivity(intent)
+        }
+    }
+
+    private fun setupContactUsButtonClickListener() {
+        binding.ivSettingContactUs.setOnClickListener {
+            val emailAddress = getString(R.string.setting_contact_us_email)
+            val subject = R.string.setting_contact_us_email_subject
+
+            val uri = "mailto:$emailAddress?subject=${Uri.encode(subject.toString())}".toUri()
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+
+            startActivity(intent)
+        }
     }
 }
