c74af4edf3002652c85e031e60056447f76a7462
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-08-07 17:10:06 +0900

    refactor: OrganizationResponse 파일 경로 변경 및 필드명 수정

--- android/app/src/main/java/com/daedan/festabook/data/datasource/remote/organization/FestivalDataSource.kt
+++ android/app/src/main/java/com/daedan/festabook/data/datasource/remote/organization/FestivalDataSource.kt
@@ -1,7 +1,7 @@
 package com.daedan.festabook.data.datasource.remote.organization
 
 import com.daedan.festabook.data.datasource.remote.ApiResult
-import com.daedan.festabook.data.model.response.OrganizationResponse
+import com.daedan.festabook.data.model.response.festival.OrganizationResponse
 
 interface FestivalDataSource {
     suspend fun fetchFestival(): ApiResult<OrganizationResponse>
