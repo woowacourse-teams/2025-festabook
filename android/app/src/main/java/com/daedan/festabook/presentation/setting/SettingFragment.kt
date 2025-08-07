fb3b8f817de7996f9ffef0875deae13a1f3fbbff
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-08-07 21:54:31 +0900

    feat: 설정 화면에 서비스 이용약관 및 문의하기 기능 추가

--- android/app/src/main/java/com/daedan/festabook/presentation/setting/SettingFragment.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/setting/SettingFragment.kt
@@ -1,7 +1,10 @@
 package com.daedan.festabook.presentation.setting
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import androidx.core.net.toUri
 import com.daedan.festabook.R
 import com.daedan.festabook.databinding.FragmentSettingBinding
 import com.daedan.festabook.presentation.common.BaseFragment
@@ -12,5 +15,28 @@ class SettingFragment : BaseFragment<FragmentSettingBinding>(R.layout.fragment_s
         savedInstanceState: Bundle?,
     ) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupServicePolicyClickListener()
+        setupContactUsButtonClickListener()
+    }
+
+    private fun setupServicePolicyClickListener() {
+        binding.ivSettingServicePolicy.setOnClickListener {
+            val url = "https://www.notion.so/244a540dc0b780638e56e31c4bdb3c9f"
+            val intent = Intent(Intent.ACTION_VIEW, url.toUri())
+            startActivity(intent)
+        }
+    }
+
+    private fun setupContactUsButtonClickListener() {
+        binding.ivSettingContactUs.setOnClickListener {
+            val emailAddress = "festabook2025@gmail.com"
+            val subject = "문의사항"
+
+            val uri = "mailto:$emailAddress?subject=${Uri.encode(subject)}".toUri()
+            val intent = Intent(Intent.ACTION_SENDTO, uri)
+
+            startActivity(intent)
+        }
     }
 }
