fb00c5abab2951c0e0ae8369cce2a2ad730a71cd
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-08-08 10:14:17 +0900

    refactor: 홈 화면 축제 날짜 형식 변경

--- android/app/src/main/java/com/daedan/festabook/presentation/common/TextUtil.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/common/TextUtil.kt
@@ -7,9 +7,9 @@ fun formatFestivalPeriod(
     end: LocalDate,
 ): String =
     if (start.year == end.year) {
-        "${start.year}. ${start.monthValue}. ${start.dayOfMonth} - " +
-            "${end.monthValue}. ${end.dayOfMonth}"
+        "${start.year}년 ${start.monthValue}월 ${start.dayOfMonth}일 ~ " +
+            "${end.monthValue}월 ${end.dayOfMonth}일"
     } else {
-        "${start.year}. ${start.monthValue}. ${start.dayOfMonth} - " +
-            "${end.year}. ${end.monthValue}. ${end.dayOfMonth}"
+        "${start.year}년 ${start.monthValue}월 ${start.dayOfMonth}일 ~ " +
+            "${end.year}년 ${end.monthValue}월 ${end.dayOfMonth}일"
     }
