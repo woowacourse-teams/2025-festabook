270cfdd5cbf35df6e2003c61a304e3a31320dbf0
changui <122252160+changuii@users.noreply.github.com>
2025-08-08 12:35:59 +0900

    [BE] 로그 추가 및 로그 파일 appender cd 로직에 추가 (#412)
    * feat: 필터 로깅 추가
    
    - MDC, 요청 관련 정보 로깅 추가했습니다.
    
    * feat: 모든 api들과 메서드들에 로그 추가
    
    * cicd: 테스트용 PR 트리거 추가 및 워크 플로우 중 logback-appender 추가
    
    * cicd: logback secrets을 대문자로 변경
    
    * fix: 테스트에서 사용하는 inner private 클래스를 public으로 변경
    
    * fix: xml 줄바꿈이 깨지지 않도록 printf로 변경
    
    * fix: nohup log 제거
    
    * fix: cd 스크립트 폴더 경로 수정
    
    * fix: cd 스크립트 과정 중에 logback-spring을 삽입하는 과정을 프로젝트와 함께 logback-spring을 관리하도록 변경
    
    * cicd: AWS cd 스크립트에서 스프링을 실행할 때 타임존을 한국으로 설정
    
    * docs: 불필요한 로그 정보 제거
    
    * refactor: 줄바꿈, 클래스 파일 마지막 개행 추가
    
    * cicd: PR 트리거 제거
    
    * remove: .DS_Store 제거
    
    * chore: gitignore에 .DS_Store 추가
    
    * chore: gitignore 마지막 줄 개행 추가
    
    * chore: .DS_Store 모든 디렉터리에서 제거
    
    * chore: DsStore 제거
    
    ---------
    
    Co-authored-by: taek2222 <oot309@daum.net>
    Co-authored-by: 창의 <chang-ui@chang-uiui-MacBookAir.local>

---
+++ backend/src/main/java/com/daedan/festabook/global/logging/LoggingAspect.java
@@ -0,0 +1,45 @@
+package com.daedan.festabook.global.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Around("""
+        within(@org.springframework.web.bind.annotation.RestController *) || 
+        within(@org.springframework.stereotype.Service *) ||
+        within(@org.springframework.stereotype.Repository *)) """
+    )
+    public Object allLayersLogging(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        log.info("[Method Call] | Class: {} | Method: {} | Args: {}",
+                joinPoint.getSignature().getName(),
+                joinPoint.getSignature().getName(),
+                joinPoint.getArgs()
+        );
+
+        long start = System.currentTimeMillis();
+        Object result = null;
+        try {
+            result = joinPoint.proceed();
+        } finally {
+            long end = System.currentTimeMillis();
+            long executionTime = end - start;
+
+            log.info("[Method End] | Class: {} | Method: {} | Execution Time: {}ms | Response: {}",
+                    joinPoint.getSignature().getDeclaringTypeName(),
+                    joinPoint.getSignature().getName(),
+                    executionTime,
+                    result
+            );
+        }
+
+        return result;
+    }
+}
