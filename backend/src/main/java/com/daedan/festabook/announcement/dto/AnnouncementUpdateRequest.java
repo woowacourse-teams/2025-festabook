3761d2ce78fbd8e89dfdb03d2e9345707144d23e
soeun2537 <soeun2537@gmail.com>
2025-08-09 19:18:51 +0900

    docs: Request DTO 문서화 추가

--- backend/src/main/java/com/daedan/festabook/announcement/dto/AnnouncementUpdateRequest.java
+++ backend/src/main/java/com/daedan/festabook/announcement/dto/AnnouncementUpdateRequest.java
@@ -1,7 +1,13 @@
 package com.daedan.festabook.announcement.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record AnnouncementUpdateRequest(
+
+        @Schema(description = "공지 제목", example = "폭우가 내립니다.")
         String title,
+
+        @Schema(description = "공지 내용", example = "우산을 챙겨주세요.")
         String content
 ) {
 }
