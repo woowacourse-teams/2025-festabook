df42913eed31dfb5bbff133c034ffd1b13f7ce74
eoehd1ek <eoehd2ek@gmail.com>
2025-08-11 06:38:51 +0900

    refactor: 미사용 롬복 어노테이션 삭제

--- backend/src/main/java/com/daedan/festabook/global/config/S3ClientConfig.java
+++ backend/src/main/java/com/daedan/festabook/global/config/S3ClientConfig.java
@@ -1,6 +1,5 @@
 package com.daedan.festabook.global.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +9,6 @@ import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 @Profile("prod")
-@RequiredArgsConstructor
 public class S3ClientConfig {
 
     @Value("${cloud.aws.region.static}")
