ae0d204b8891748d5fa99ff5cd0d7d9ca6346a8d
eoehd1ek <eoehd2ek@gmail.com>
2025-08-12 23:17:47 +0900

    test: getBytes() 테스트 추가

--- backend/src/test/java/com/daedan/festabook/storage/dto/StorageUploadRequestTest.java
+++ backend/src/test/java/com/daedan/festabook/storage/dto/StorageUploadRequestTest.java
@@ -1,8 +1,10 @@
 package com.daedan.festabook.storage.dto;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.daedan.festabook.global.exception.BusinessException;
+import java.io.IOException;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
@@ -78,6 +80,51 @@ class StorageUploadRequestTest {
             assertThatThrownBy(() -> new StorageUploadRequest(file, filePath))
                     .isInstanceOf(BusinessException.class)
                     .hasMessage("파일 경로는 %d자를 초과할 수 없습니다.", maxFileNameLength);
+        }
+    }
+
+    @Nested
+    class getBytes {
+
+        @Test
+        void 성공_파일_바이트_읽기() throws IOException {
+            // given
+            byte[] fileContent = new byte[10];
+            MultipartFile file = new MockMultipartFile(
+                    "file",
+                    "test.jpg",
+                    "image/jpeg",
+                    fileContent
+            );
+            String filePath = "test.jpg";
+
+            // when
+            byte[] result = new StorageUploadRequest(file, filePath).getBytes();
+
+            // then
+            assertThat(result).isEqualTo(fileContent);
+        }
+
+        @Test
+        void 예외_파일_바이트_읽기_실패() {
+            // given
+            MultipartFile file = new MockMultipartFile(
+                    "file",
+                    "test.jpg",
+                    "image/jpeg",
+                    new byte[10]
+            ) {
+                @Override
+                public byte[] getBytes() throws IOException {
+                    throw new IOException();
+                }
+            };
+            String filePath = "test.jpg";
+
+            // when & then
+            assertThatThrownBy(() -> new StorageUploadRequest(file, filePath).getBytes())
+                    .isInstanceOf(BusinessException.class)
+                    .hasMessage("MultipartFile 에서 Byte 데이터를 읽기 실패했습니다.");
         }
     }
 }
