3b6ce1391827d3a55d127ffac26475ca4412604d
oungsi2000 <oungsi1000@gmail.com>
2025-08-13 12:22:26 +0900

    refactor: 중복된 뷰 계층 단일화 및 성능 최적화

--- android/app/src/main/java/com/daedan/festabook/presentation/common/CategoryView.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/common/CategoryView.kt
@@ -6,84 +6,88 @@ import android.view.LayoutInflater
 import androidx.annotation.ColorRes
 import androidx.core.graphics.ColorUtils
 import androidx.databinding.BindingAdapter
-import androidx.databinding.DataBindingUtil
 import com.daedan.festabook.R
 import com.daedan.festabook.databinding.CategoryViewBinding
 import com.daedan.festabook.presentation.placeList.model.PlaceCategoryUiModel
 import com.google.android.material.card.MaterialCardView
 import kotlin.math.roundToInt
 
-class CategoryView(
-    context: Context,
-    attrs: AttributeSet? = null,
-) : MaterialCardView(context, attrs) {
-    private val binding: CategoryViewBinding =
-        DataBindingUtil.inflate(
-            LayoutInflater.from(context),
-            R.layout.category_view,
-            this,
-            true,
-        )
+class CategoryView
+    @JvmOverloads
+    constructor(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0,
+    ) : MaterialCardView(context, attrs, defStyleAttr) {
+        private val binding: CategoryViewBinding =
+            CategoryViewBinding.inflate(LayoutInflater.from(context), this, true)
 
-    init {
-        radius = 6f
-        cardElevation = 0f
-        strokeWidth = 0
-    }
-
-    fun setCategory(category: PlaceCategoryUiModel) {
-        when (category) {
-            PlaceCategoryUiModel.FOOD_TRUCK -> {
-                binding.tvPlaceCategory.text =
-                    context.getString(R.string.place_list_title_food_truck)
-                binding.ivCategoryIcon.setImageResource(R.drawable.ic_map_category_food_truck)
-                binding.cvPlaceCategory.setCardBackgroundColor(
-                    getAlphaColor(R.color.green400),
-                )
-            }
-
-            PlaceCategoryUiModel.BAR -> {
-                binding.tvPlaceCategory.text =
-                    context.getString(R.string.place_list_title_bar)
-                binding.ivCategoryIcon.setImageResource(R.drawable.ic_map_category_bar)
-                binding.cvPlaceCategory.setCardBackgroundColor(
-                    getAlphaColor(R.color.orange400),
-                )
-            }
-
-            PlaceCategoryUiModel.BOOTH -> {
-                binding.tvPlaceCategory.text =
-                    context.getString(R.string.place_list_title_booth)
-                binding.ivCategoryIcon.setImageResource(R.drawable.ic_map_category_booth)
-                binding.cvPlaceCategory.setCardBackgroundColor(
-                    getAlphaColor(R.color.blue400),
-                )
-            }
-
-            PlaceCategoryUiModel.SMOKING_AREA, PlaceCategoryUiModel.TOILET, PlaceCategoryUiModel.TRASH_CAN -> Unit
+        init {
+            radius = 6f
+            cardElevation = 0f
+            strokeWidth = 0
         }
-    }
 
-    private fun getAlphaColor(
-        @ColorRes colorRes: Int,
-    ): Int {
-        val original = context.getColor(colorRes)
-        val alpha = (MAX_ALPHA * ALPHA_RATIO).roundToInt()
-        return ColorUtils.setAlphaComponent(original, alpha)
-    }
+        fun setCategory(category: PlaceCategoryUiModel) {
+            when (category) {
+                PlaceCategoryUiModel.FOOD_TRUCK -> {
+                    updateUI(
+                        R.string.place_list_title_food_truck,
+                        R.drawable.ic_map_category_food_truck,
+                        R.color.green400,
+                    )
+                }
 
-    companion object {
-        private const val MAX_ALPHA = 255
-        private const val ALPHA_RATIO = 0.10f
+                PlaceCategoryUiModel.BAR -> {
+                    updateUI(
+                        R.string.place_list_title_bar,
+                        R.drawable.ic_map_category_bar,
+                        R.color.orange400,
+                    )
+                }
 
-        @JvmStatic
-        @BindingAdapter("category")
-        fun setCategory(
-            view: CategoryView,
-            category: PlaceCategoryUiModel?,
+                PlaceCategoryUiModel.BOOTH -> {
+                    updateUI(
+                        R.string.place_list_title_booth,
+                        R.drawable.ic_map_category_booth,
+                        R.color.blue400,
+                    )
+                }
+
+                PlaceCategoryUiModel.SMOKING_AREA, PlaceCategoryUiModel.TOILET, PlaceCategoryUiModel.TRASH_CAN -> Unit
+            }
+        }
+
+        private fun updateUI(
+            titleResId: Int,
+            iconResId: Int,
+            colorResId: Int,
         ) {
-            category ?: return
-            view.setCategory(category)
+            binding.tvPlaceCategory.text = context.getString(titleResId)
+            binding.ivCategoryIcon.setImageResource(iconResId)
+            setCardBackgroundColor(getAlphaColor(colorResId))
+        }
+
+        private fun getAlphaColor(
+            @ColorRes colorRes: Int,
+        ): Int {
+            val original = context.getColor(colorRes)
+            val alpha = (MAX_ALPHA * ALPHA_RATIO).roundToInt()
+            return ColorUtils.setAlphaComponent(original, alpha)
+        }
+
+        companion object {
+            private const val MAX_ALPHA = 255
+            private const val ALPHA_RATIO = 0.10f
+
+            @JvmStatic
+            @BindingAdapter("category")
+            fun setCategory(
+                view: CategoryView,
+                category: PlaceCategoryUiModel?,
+            ) {
+                category ?: return
+                view.setCategory(category)
+            }
         }
     }
-}
