fa0400ff04660be2813707232ede4d51e03e513e
changuii <rhljh7410@gmail.com>
2025-08-13 22:02:29 +0900

    refactor: Request의 설명을 다시 작성

--- backend/src/main/java/com/daedan/festabook/place/dto/PlaceImageRequest.java
+++ backend/src/main/java/com/daedan/festabook/place/dto/PlaceImageRequest.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record PlaceImageRequest(
 
-        @Schema(description = "플레이스 좌표 위도", example = "https://festabook.net/image/poster.jpg")
+        @Schema(description = "플레이스 이미지 url", example = "https://festabook.net/image/poster.jpg")
         String imageUrl
 ) {
 }
