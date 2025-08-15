34c3b4056f6428355cc066a815dd70dc422d882a
oungsi2000 <oungsi1000@gmail.com>
2025-08-15 18:28:13 +0900

    feat: PlaceDetailActivity에서 뒤로가기 버튼추가

--- android/app/src/main/java/com/daedan/festabook/presentation/placeDetail/PlaceDetailActivity.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/placeDetail/PlaceDetailActivity.kt
@@ -77,6 +77,9 @@ class PlaceDetailActivity :
         binding.vpPlaceImages.adapter = placeImageAdapter
         binding.tvLocation.setExpandedWhenClicked()
         binding.tvHost.setExpandedWhenClicked()
+        binding.ivBackToPrevious.setOnClickListener {
+            finish()
+        }
     }
 
     private fun setUpObserver() {
