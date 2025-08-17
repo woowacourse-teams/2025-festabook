013e27b3ccf9c1ef334e64671c6867e4ae0ea346
박지민 <pjm5489@naver.com>
2025-08-17 21:06:06 +0900

    feat: 난독화 적용

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -103,7 +103,8 @@ android {
         }
 
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro",
