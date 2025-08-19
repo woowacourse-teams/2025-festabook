e507b88fab098a7117d773f7b4e1a08d20832b4a
soeun2537 <soeun2537@gmail.com>
2025-08-19 19:56:35 +0900

    feat: CouncilDetails 구현

---
+++ backend/src/main/java/com/daedan/festabook/global/security/council/CouncilDetails.java
@@ -0,0 +1,56 @@
+package com.daedan.festabook.global.security.council;
+
+import com.daedan.festabook.council.domain.Council;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class CouncilDetails implements UserDetails {
+
+    private final Council council;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Set<String> roles = council.getRoles();
+        return roles.stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public String getPassword() {
+        return council.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return council.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- backend/src/main/java/com/daedan/festabook/global/security/council/CouncilDetailsService.java
+++ backend/src/main/java/com/daedan/festabook/global/security/council/CouncilDetailsService.java
@@ -1,12 +1,10 @@
-package com.daedan.festabook.council.service;
+package com.daedan.festabook.global.security.council;
 
 import com.daedan.festabook.council.domain.Council;
 import com.daedan.festabook.council.infrastructure.CouncilJpaRepository;
 import com.daedan.festabook.global.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -23,12 +21,6 @@ public class CouncilDetailsService implements UserDetailsService {
         Council council = councilJpaRepository.findByUsername(username)
                 .orElseThrow(() -> new BusinessException("존재하지 않는 학생회입니다.", HttpStatus.NOT_FOUND));
 
-        return User.builder()
-                .username(council.getUsername())
-                .password(council.getPassword())
-                .authorities(council.getRoles().stream()
-                        .map(SimpleGrantedAuthority::new)
-                        .toList())
-                .build();
+        return new CouncilDetails(council);
     }
 }
