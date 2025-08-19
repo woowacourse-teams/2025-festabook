bcd12e918d7d192567c617cef811275c90fa68be
soeun2537 <soeun2537@gmail.com>
2025-08-19 13:24:36 +0900

    feat: Council 검증 추가

--- backend/src/main/java/com/daedan/festabook/council/domain/Council.java
+++ backend/src/main/java/com/daedan/festabook/council/domain/Council.java
@@ -1,5 +1,6 @@
 package com.daedan.festabook.council.domain;
 
+import com.daedan.festabook.global.exception.BusinessException;
 import jakarta.persistence.Column;
 import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
@@ -12,6 +13,7 @@ import java.util.Set;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Entity
 @Getter
@@ -22,7 +24,7 @@ public class Council {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String username;
 
     @Column(nullable = false)
@@ -30,4 +32,40 @@ public class Council {
 
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<String> roles = new HashSet<>();
+
+    protected Council(
+            Long id,
+            String username,
+            String password
+    ) {
+        validateUsername(username);
+        validatePassword(password);
+
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Council(
+            String username,
+            String password
+    ) {
+        this(
+                null,
+                username,
+                password
+        );
+    }
+
+    private void validateUsername(String username) {
+        if (username == null || username.trim().isEmpty()) {
+            throw new BusinessException("아이디는 비어 있을 수 없습니다.", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private void validatePassword(String password) {
+        if (password == null || password.trim().isEmpty()) {
+            throw new BusinessException("비밀번호는 비어 있을 수 없습니다.", HttpStatus.BAD_REQUEST);
+        }
+    }
 }
