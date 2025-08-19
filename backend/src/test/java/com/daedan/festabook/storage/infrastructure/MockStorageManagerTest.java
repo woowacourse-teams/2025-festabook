aaf1790cccb24ce329ab3d85f33945c479f14ec5
eoehd1ek <eoehd2ek@gmail.com>
2025-08-19 11:31:25 +0900

    feat: 테스트 환경용 모킹 StorageManager 추가

---
+++ backend/src/test/java/com/daedan/festabook/storage/infrastructure/MockStorageManagerTest.java
@@ -0,0 +1,41 @@
+package com.daedan.festabook.storage.infrastructure;
+
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+
+import com.daedan.festabook.storage.dto.StorageUploadRequest;
+import com.daedan.festabook.storage.dto.StorageUploadResponse;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.mock.web.MockMultipartFile;
+
+class MockStorageManagerTest {
+
+    private final MockStorageManager mockStorageManager = new MockStorageManager();
+
+    @Nested
+    class uploadFile {
+
+        @Test
+        void 성공() {
+            // given
+            String fileName = "test_123.png";
+            String storagePath = "mock-path/test_123.png";
+            MockMultipartFile mockFile = new MockMultipartFile(
+                    "file",
+                    fileName,
+                    "image/png",
+                    new byte[10]
+            );
+            StorageUploadRequest request = new StorageUploadRequest(mockFile, storagePath);
+
+            // when
+            StorageUploadResponse response = mockStorageManager.uploadFile(request);
+
+            // then
+            assertSoftly(s -> {
+                s.assertThat(response.accessUrl()).isNotBlank();
+                s.assertThat(response.storagePath()).isEqualTo(storagePath);
+            });
+        }
+    }
+}
