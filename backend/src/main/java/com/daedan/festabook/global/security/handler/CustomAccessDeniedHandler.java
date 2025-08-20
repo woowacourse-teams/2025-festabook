0783ecc9dc621e55e969c44018c75c0ab3c9dbad
soeun2537 <soeun2537@gmail.com>
2025-08-20 22:02:32 +0900

    refactor: 로그 삭제

--- backend/src/main/java/com/daedan/festabook/global/security/handler/CustomAccessDeniedHandler.java
+++ backend/src/main/java/com/daedan/festabook/global/security/handler/CustomAccessDeniedHandler.java
@@ -4,12 +4,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
@@ -17,8 +15,6 @@ public class CustomAccessDeniedHandler implements AccessDeniedHandler {
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception)
             throws IOException, ServletException {
         // TODO: Log 병합 후 형식 수정
-        log.error("Access Denied: ", exception);
-
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.setContentType("application/json;charset=UTF-8");
         response.getWriter().print("{\"errorCode\": \"403\", \"errorMessage\": \"접근 권한이 없습니다.\"}");
--- backend/src/main/java/com/daedan/festabook/global/security/handler/CustomAuthenticationEntryPoint.java
+++ backend/src/main/java/com/daedan/festabook/global/security/handler/CustomAuthenticationEntryPoint.java
@@ -4,12 +4,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
@@ -17,8 +15,6 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
             throws IOException, ServletException {
         // TODO: Log 병합 후 형식 수정
-        log.error("Authentication failed: ", exception);
-
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType("application/json;charset=UTF-8");
         response.getWriter().print("{\"errorCode\": \"401\", \"errorMessage\": \"인증이 필요합니다.\"}");
