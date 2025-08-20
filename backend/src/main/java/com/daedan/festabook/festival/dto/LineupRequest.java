293b11f41856595645d98375fc6aa110f875ec0e
taek2222 <oot309@daum.net>
2025-08-20 19:34:23 +0900

    feat: Swagger 문서화 등록

--- backend/src/main/java/com/daedan/festabook/festival/dto/LineupRequest.java
+++ backend/src/main/java/com/daedan/festabook/festival/dto/LineupRequest.java
@@ -2,11 +2,18 @@ package com.daedan.festabook.festival.dto;
 
 import com.daedan.festabook.festival.domain.Festival;
 import com.daedan.festabook.festival.domain.Lineup;
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.time.LocalDateTime;
 
 public record LineupRequest(
+
+        @Schema(description = "라인업 인물 이름", example = "이미소")
         String name,
+
+        @Schema(description = "라인업 인물 이미지", example = "http://example.com/image.png")
         String imageUrl,
+
+        @Schema(description = "라인업 일정", example = "2025-10-15T12:00:00")
         LocalDateTime performanceAt
 ) {
 
