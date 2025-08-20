5d9002f29914cb6677c0a8f1b411300ab877c3c0
soeun2537 <soeun2537@gmail.com>
2025-08-20 14:48:07 +0900

    feat: JWT 반영

--- backend/src/test/java/com/daedan/festabook/storage/controller/ImageStoreControllerTest.java
+++ backend/src/test/java/com/daedan/festabook/storage/controller/ImageStoreControllerTest.java
@@ -3,6 +3,10 @@ package com.daedan.festabook.storage.controller;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
+import com.daedan.festabook.festival.domain.Festival;
+import com.daedan.festabook.festival.domain.FestivalFixture;
+import com.daedan.festabook.festival.infrastructure.FestivalJpaRepository;
+import com.daedan.festabook.global.security.JwtTestHelper;
 import io.restassured.RestAssured;
 import java.io.IOException;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +14,7 @@ import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -18,6 +23,15 @@ import org.springframework.mock.web.MockMultipartFile;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class ImageStoreControllerTest {
+
+    private static final String AUTHENTICATION_HEADER = "Authorization";
+    private static final String AUTHENTICATION_SCHEME = "Bearer ";
+
+    @Autowired
+    private FestivalJpaRepository festivalJpaRepository;
+
+    @Autowired
+    private JwtTestHelper jwtTestHelper;
 
     @LocalServerPort
     private int port;
@@ -33,6 +47,11 @@ class ImageStoreControllerTest {
         @Test
         void 성공() throws IOException {
             // given
+            Festival festival = FestivalFixture.create();
+            festivalJpaRepository.save(festival);
+
+            String token = jwtTestHelper.createCouncilAndLogin(festival);
+
             MockMultipartFile file = new MockMultipartFile(
                     "image",
                     "sample.png",
@@ -45,6 +64,7 @@ class ImageStoreControllerTest {
             // when & then
             RestAssured
                     .given()
+                    .header(AUTHENTICATION_HEADER, AUTHENTICATION_SCHEME + token)
                     .multiPart(
                             "image",
                             file.getOriginalFilename(),
