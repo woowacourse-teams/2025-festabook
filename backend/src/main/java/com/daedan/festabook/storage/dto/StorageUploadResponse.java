28b3f6a6a50f15ee1716148e04e51f908bd702b2
Dae-dong <49092390+eoehd1ek@users.noreply.github.com>
2025-08-21 00:34:51 +0900

    [BE] cloudFront 경유해 s3를 접근하도록 상대경로 응답 변경 (#589)
    * refactor: cloudFront 경유해 s3를 접근하도록 상대경로 응답 변경
    
    * refactor: 변수 이름 수정

--- backend/src/main/java/com/daedan/festabook/storage/dto/StorageUploadResponse.java
+++ backend/src/main/java/com/daedan/festabook/storage/dto/StorageUploadResponse.java
@@ -2,6 +2,7 @@ package com.daedan.festabook.storage.dto;
 
 public record StorageUploadResponse(
         String accessUrl,
+        String accessRelativePath,
         String storagePath
 ) {
 }
--- backend/src/main/java/com/daedan/festabook/storage/infrastructure/MockStorageManager.java
+++ backend/src/main/java/com/daedan/festabook/storage/infrastructure/MockStorageManager.java
@@ -15,6 +15,19 @@ public class MockStorageManager implements StorageManager {
     @Override
     public StorageUploadResponse uploadFile(StorageUploadRequest request) {
         StoreFile storeFile = new StoreFile(request.file(), request.storagePath());
-        return new StorageUploadResponse(MOCK_ACCESS_URL, storeFile.getStoragePath());
+
+        StorageUploadResponse response = new StorageUploadResponse(
+                MOCK_ACCESS_URL,
+                ensureLeadingSlash(storeFile.getStoragePath()),
+                storeFile.getStoragePath()
+        );
+        return response;
+    }
+
+    private String ensureLeadingSlash(String storagePath) {
+        if (storagePath.startsWith("/")) {
+            return storagePath;
+        }
+        return String.format("/%s", storagePath);
     }
 }
