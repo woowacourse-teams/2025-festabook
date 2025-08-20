012dc8ee7d920d3f21ee485e6b627d9aacb42146
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-08-20 11:05:02 +0900

    refactor: 사용하지 않는 parelable 속성 제거

--- android/app/src/main/java/com/daedan/festabook/presentation/home/LineupItemUiModel.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/home/LineupItemUiModel.kt
@@ -1,15 +1,12 @@
 package com.daedan.festabook.presentation.home
 
-import android.os.Parcelable
 import com.daedan.festabook.domain.model.LineupItem
-import kotlinx.parcelize.Parcelize
 
-@Parcelize
 data class LineupItemUiModel(
     val id: Long,
     val imageUrl: String,
     val name: String,
-) : Parcelable
+)
 
 fun LineupItem.toUiModel(): LineupItemUiModel =
     LineupItemUiModel(
