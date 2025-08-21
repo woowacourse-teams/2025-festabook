abf0122584cb8e6e4288911d9735f879ade24fb6
soeun2537 <soeun2537@gmail.com>
2025-08-21 21:12:22 +0900

    refactor: ObjectMapper 추가

--- backend/src/main/java/com/daedan/festabook/global/security/handler/CustomAuthenticationEntryPoint.java
+++ backend/src/main/java/com/daedan/festabook/global/security/handler/CustomAuthenticationEntryPoint.java
@@ -1,24 +1,48 @@
 package com.daedan.festabook.global.security.handler;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
+@RequiredArgsConstructor
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
             throws IOException, ServletException {
-        // TODO: Log 병합 후 형식 수정
+
+        log.info("Security 인증 실패: 요청 경로={}, 메서드={}, 에러 메시지={}",
+                request.getRequestURI(),
+                request.getMethod(),
+                exception.getMessage(), exception);
+
+        AuthenticationErrorResponse errorResponse = new AuthenticationErrorResponse(
+                String.valueOf(HttpStatus.UNAUTHORIZED.value()),
+                "인증이 필요합니다."
+        );
+
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
         response.setContentType(MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8");
-        response.getWriter().print("{\"errorCode\": \"401\", \"errorMessage\": \"인증이 필요합니다.\"}");
+        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
         response.getWriter().flush();
+    }
+
+    private record AuthenticationErrorResponse(
+            String errorCode,
+            String errorMessage
+    ) {
     }
 }
