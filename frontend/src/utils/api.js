f1dc2da94cc657f24049aa8ad665eab5d31e2182
changui <122252160+changuii@users.noreply.github.com>
2025-08-21 22:08:58 +0900

    fix: api_host를 env를 사용하도록 변경

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,8 +1,7 @@
 // src/utils/api.js
 import axios from 'axios';
 
-const API_HOST = 'http://localhost/api';
-// import.meta.env.VITE_API_HOST;
+const API_HOST = import.meta.env.VITE_API_HOST;
 const api = axios.create({
   baseURL: API_HOST,
   headers: {
