ea626f3bb97b8ba5469e2537b54d83c32eca8ff0
eoehd1ek <eoehd2ek@gmail.com>
2025-08-21 13:02:33 +0900

    feat: dev 환경 로컬 저장소 추가

---
+++ backend/src/main/java/com/daedan/festabook/storage/infrastructure/LocalStorageManager.java
@@ -0,0 +1,65 @@
+package com.daedan.festabook.storage.infrastructure;
+
+import com.daedan.festabook.storage.domain.StorageManager;
+import com.daedan.festabook.storage.dto.StorageUploadRequest;
+import com.daedan.festabook.storage.dto.StorageUploadResponse;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Profile("dev")
+public class LocalStorageManager implements StorageManager {
+
+    private final String basePath;
+
+    public LocalStorageManager(
+            @Value("${local.storage.base-path}") String basePath
+    ) {
+        this.basePath = basePath;
+    }
+
+    @Override
+    public StorageUploadResponse uploadFile(StorageUploadRequest request) {
+        StoreFile storeFile = new StoreFile(request.file(), request.storagePath());
+
+        try {
+            Path fileStorePath = Paths.get(basePath, storeFile.getStoragePath());
+            createDirectoryIfNotExists(fileStorePath.getParent());
+
+            request.file().transferTo(fileStorePath);
+
+            StorageUploadResponse response = new StorageUploadResponse(
+                    ensureLeadingSlash(storeFile.getStoragePath()),
+                    ensureLeadingSlash(storeFile.getStoragePath()),
+                    request.storagePath()
+            );
+            return response;
+        } catch (IOException e) {
+            throw new RuntimeException(String.format(
+                    "로컬 저장소 파일 저장 실패 %s",
+                    e.getMessage()
+            ));
+        }
+    }
+
+    private void createDirectoryIfNotExists(Path directoryPath) throws IOException {
+        if (directoryPath != null && !Files.exists(directoryPath)) {
+            Files.createDirectories(directoryPath);
+            log.info("디렉토리가 생성되었습니다: {}", directoryPath);
+        }
+    }
+
+    private String ensureLeadingSlash(String storagePath) {
+        if (storagePath.startsWith("/")) {
+            return storagePath;
+        }
+        return String.format("/%s", storagePath);
+    }
+}
---
+++ backend/src/test/java/com/daedan/festabook/storage/infrastructure/LocalStorageManagerTest.java
@@ -0,0 +1,91 @@
+package com.daedan.festabook.storage.infrastructure;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.willThrow;
+import static org.mockito.Mockito.mock;
+
+import com.daedan.festabook.storage.dto.StorageUploadRequest;
+import com.daedan.festabook.storage.dto.StorageUploadRequestFixture;
+import com.daedan.festabook.storage.dto.StorageUploadResponse;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class LocalStorageManagerTest {
+
+    @TempDir
+    private Path tempDir;
+
+    private LocalStorageManager localStorageManager;
+
+    @BeforeEach
+    void setUp() {
+        localStorageManager = new LocalStorageManager(tempDir.toString());
+    }
+
+    @Nested
+    class uploadFile {
+
+        @Test
+        void 성공() {
+            // given
+            String fileName = "sample.png";
+            MockMultipartFile mockFile = new MockMultipartFile(
+                    "file",
+                    fileName,
+                    "image/png",
+                    new byte[10]
+            );
+            String storagePath = "images/" + fileName;
+            String expectedAccessUrl = "/" + storagePath;
+            String expectedRelativePath = "/" + storagePath;
+            StorageUploadRequest request = StorageUploadRequestFixture.create(mockFile, storagePath);
+
+            // when
+            StorageUploadResponse response = localStorageManager.uploadFile(request);
+
+            // then
+            assertSoftly(s -> {
+                s.assertThat(response.accessUrl()).isEqualTo(expectedAccessUrl);
+                s.assertThat(response.accessRelativePath()).startsWith(expectedRelativePath);
+                s.assertThat(response.storagePath()).isEqualTo(storagePath);
+            });
+            Path expectedFilePath = tempDir.resolve(response.storagePath());
+            assertThat(Files.exists(expectedFilePath)).isTrue();
+        }
+
+        @Test
+        void 예외_파일_저장_실패시_RuntimeException_변환() throws IOException {
+            // given
+            // 파일 데이터를 비워서 IOException을 발생시킴
+            MultipartFile mockFile = mock(MultipartFile.class);
+            willThrow(IOException.class)
+                    .given(mockFile)
+                    .transferTo(any(Path.class));
+
+            String storagePath = "images/sample.png";
+            StorageUploadRequest request = new StorageUploadRequest(mockFile, storagePath);
+
+            // when & then
+            // RuntimeException이 발생하는지 검증
+            assertThatThrownBy(() -> localStorageManager.uploadFile(request))
+                    .isInstanceOf(RuntimeException.class)
+                    .hasMessageContaining("로컬 저장소 파일 저장 실패");
+        }
+    }
+}
