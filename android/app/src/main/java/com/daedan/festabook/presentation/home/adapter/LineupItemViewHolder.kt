83623ad6847c091a1c67f38d0c50aa57429973a6
Dongjoo Seo <83579348+etama123@users.noreply.github.com>
2025-08-22 09:30:09 +0900

    refactor: 이미지 로드 로직 공통화
    Coil의 `load` 함수를 직접 사용하는 대신, `loadImage`라는 확장 함수를 만들어 이미지 로드 로직을 재사용 가능하도록 변경했습니다.
    
    `LineupItemViewHolder`에서 `itemLineupImage.load`를 `itemLineupImage.loadImage`로 수정했습니다.

--- android/app/src/main/java/com/daedan/festabook/presentation/home/adapter/LineupItemViewHolder.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/home/adapter/LineupItemViewHolder.kt
@@ -5,19 +5,17 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.graphics.drawable.toDrawable
 import androidx.recyclerview.widget.RecyclerView
-import coil3.load
 import coil3.request.CachePolicy
 import coil3.request.placeholder
 import com.daedan.festabook.databinding.ItemHomeLineupBinding
+import com.daedan.festabook.presentation.common.loadImage
 import com.daedan.festabook.presentation.home.LineupItemUiModel
-import timber.log.Timber
 
 class LineupItemViewHolder(
     val binding: ItemHomeLineupBinding,
 ) : RecyclerView.ViewHolder(binding.root) {
     fun bind(lineupItemUiModel: LineupItemUiModel) {
-        Timber.d("lineupItemUiModel: $lineupItemUiModel")
-        binding.itemLineupImage.load(lineupItemUiModel.imageUrl) {
+        binding.itemLineupImage.loadImage(lineupItemUiModel.imageUrl) {
             placeholder(Color.LTGRAY.toDrawable())
             memoryCachePolicy(CachePolicy.ENABLED)
         }
