34c5161dcdd9d65ec7692da44675f30835202928
박지민 <pjm5489@naver.com>
2025-08-22 10:01:24 +0900

    refactor: 가시성 변경

--- android/app/src/main/java/com/daedan/festabook/presentation/news/lost/LostItemFragment.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/news/lost/LostItemFragment.kt
@@ -105,7 +105,7 @@ class LostItemFragment : BaseFragment<FragmentLostItemBinding>(R.layout.fragment
     }
 
     private fun showSkeleton() {
-        binding.srlLostItemList.visibility = View.INVISIBLE
+        binding.srlLostItemList.visibility = View.GONE
         binding.sflLostItemSkeleton.visibility = View.VISIBLE
         binding.sflLostItemSkeleton.startShimmer()
     }
