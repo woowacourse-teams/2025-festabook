7746bf57e27fc909bea802cde8d05ef93d9aaca8
soeun2537 <soeun2537@gmail.com>
2025-08-30 21:43:33 +0900

    refactor: 미사용 메서드 삭제

--- backend/src/test/java/com/daedan/festabook/lineup/domain/LineupFixture.java
+++ backend/src/test/java/com/daedan/festabook/lineup/domain/LineupFixture.java
@@ -4,9 +4,6 @@ import com.daedan.festabook.festival.domain.Festival;
 import com.daedan.festabook.festival.domain.FestivalFixture;
 import com.daedan.festabook.global.fixture.BaseEntityTestHelper;
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class LineupFixture {
 
@@ -14,19 +11,6 @@ public class LineupFixture {
     private static final String DEFAULT_LINEUP_NAME = "이미소";
     private static final String DEFAULT_IMAGE_URL = "https://example.com/image.jpg";
     private static final LocalDateTime DEFAULT_PERFORM_AT = LocalDateTime.of(2025, 10, 15, 12, 0, 0);
-
-    public static Lineup create(
-            Long lineupId
-    ) {
-        Lineup lineup = new Lineup(
-                DEFAULT_FESTIVAL,
-                DEFAULT_LINEUP_NAME,
-                DEFAULT_IMAGE_URL,
-                DEFAULT_PERFORM_AT
-        );
-        BaseEntityTestHelper.setId(lineup, lineupId);
-        return lineup;
-    }
 
     public static Lineup create(
             Long lineupId,
@@ -50,18 +34,6 @@ public class LineupFixture {
                 DEFAULT_LINEUP_NAME,
                 DEFAULT_IMAGE_URL,
                 DEFAULT_PERFORM_AT
-        );
-    }
-
-    public static Lineup create(
-            Festival festival,
-            LocalDateTime dateTime
-    ) {
-        return new Lineup(
-                festival,
-                DEFAULT_LINEUP_NAME,
-                DEFAULT_IMAGE_URL,
-                dateTime
         );
     }
 
@@ -93,11 +65,5 @@ public class LineupFixture {
         );
         BaseEntityTestHelper.setId(lineup, lineupId);
         return lineup;
-    }
-
-    public static List<Lineup> createList(int size, Festival festival) {
-        return IntStream.range(0, size)
-                .mapToObj(i -> LineupFixture.create(festival))
-                .collect(Collectors.toList());
     }
 }
