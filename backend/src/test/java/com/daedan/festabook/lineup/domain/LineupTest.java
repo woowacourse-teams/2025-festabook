8ff1c2207e2bde93a91fa566cef7729b4132e8d7
soeun2537 <soeun2537@gmail.com>
2025-09-02 21:55:44 +0900

    test: import 변경

--- backend/src/test/java/com/daedan/festabook/lineup/domain/LineupTest.java
+++ backend/src/test/java/com/daedan/festabook/lineup/domain/LineupTest.java
@@ -1,6 +1,6 @@
 package com.daedan.festabook.lineup.domain;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import com.daedan.festabook.festival.domain.Festival;
