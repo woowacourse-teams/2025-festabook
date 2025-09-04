315cb0de4b4b8e477ec064dd5d976a7ca1fe9be6
taek2222 <oot309@daum.net>
2025-09-04 12:25:14 +0900

    chore: sudo 권한 적용

--- backend/infra/deploy.sh
+++ backend/infra/deploy.sh
@@ -18,7 +18,7 @@ fi
 
 echo "▶️ 새로운 Spring WAS 실행 중..."
 if [ -f "$JAR_NAME" ]; then
-  nohup java -jar -Duser.timezone=Asia/Seoul "$JAR_NAME" --spring.profiles.active=prod &
+  sudo nohup java -jar -Duser.timezone=Asia/Seoul "$JAR_NAME" --spring.profiles.active=prod &
   echo "📦 실행 파일: $JAR_NAME"
 else
   echo "❌ 오류: $APP_HOME 경로에서 JAR 파일을 찾을 수 없습니다."
