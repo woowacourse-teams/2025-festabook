10d492508ee53cc26404b4a2773a1f454feb6401
eoehd1ek <eoehd2ek@gmail.com>
2025-09-07 22:58:31 +0900

    feat: ConflictException 추가

---
+++ backend/src/main/java/com/daedan/festabook/global/exception/ConflictException.java
@@ -0,0 +1,19 @@
+package com.daedan.festabook.global.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class ConflictException extends BusinessException {
+
+    private final Class<?> clazz;
+
+    public ConflictException(Class<?> clazz) {
+        this(String.format("[%s] 이미 존재합니다.", clazz.getSimpleName()), clazz);
+    }
+
+    protected ConflictException(String message, Class<?> clazz) {
+        super(message, HttpStatus.CONFLICT);
+        this.clazz = clazz;
+    }
+}
---
+++ backend/src/test/java/com/daedan/festabook/global/exception/ConflictExceptionTest.java
@@ -0,0 +1,24 @@
+package com.daedan.festabook.global.exception;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class ConflictExceptionTest {
+
+    @Test
+    void 성공_getter() {
+        // given
+        Class<?> clazz = String.class;
+        String expectedMessage = "[String] 이미 존재합니다.";
+
+        // when
+        ConflictException conflictException = new ConflictException(clazz);
+
+        // then
+        assertAll(() -> assertEquals(expectedMessage, conflictException.getMessage()),
+                () -> assertEquals(clazz, conflictException.getClazz()),
+                () -> assertEquals(409, conflictException.getStatus().value()));
+    }
+}
