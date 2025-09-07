0f88c463b38811010b772cdf9d5674800633c506
eoehd1ek <eoehd2ek@gmail.com>
2025-09-07 22:58:45 +0900

    feat: NotFoundException 추가

---
+++ backend/src/test/java/com/daedan/festabook/global/exception/NotFoundExceptionTest.java
@@ -0,0 +1,26 @@
+package com.daedan.festabook.global.exception;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class NotFoundExceptionTest {
+
+    @Test
+    void 성공_getter() {
+        // given
+        Class<?> clazz = String.class;
+        String expectedMessage = "[String] 존재하지 않습니다.";
+
+        // when
+        NotFoundException notFoundException = new NotFoundException(clazz);
+
+        // then
+        assertAll(
+                () -> assertEquals(expectedMessage, notFoundException.getMessage()),
+                () -> assertEquals(clazz, notFoundException.getClazz()),
+                () -> assertEquals(404, notFoundException.getStatus().value())
+        );
+    }
+}
