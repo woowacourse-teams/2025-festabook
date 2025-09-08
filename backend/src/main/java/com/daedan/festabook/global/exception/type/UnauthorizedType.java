657cfc7e0f27b80e2b6a32b3897954656b44a5b3
eoehd1ek <eoehd2ek@gmail.com>
2025-09-08 20:00:40 +0900

    refactor: 메시지 본문 변경

--- backend/src/main/java/com/daedan/festabook/global/exception/type/UnauthorizedType.java
+++ backend/src/main/java/com/daedan/festabook/global/exception/type/UnauthorizedType.java
@@ -8,7 +8,7 @@ public enum UnauthorizedType {
     BAD_CREDENTIALS("아이디/비밀번호가 올바르지 않습니다."),
     NO_TOKEN("토큰이 없습니다."),
     EXPIRED_TOKEN("토큰이 만료되었습니다."),
-    INVALID_TOKEN("토큰이 유효성이 올바르지 않습니다.");
+    INVALID_TOKEN("토큰이 유효하지 않습니다.");
 
     private final String message;
 
