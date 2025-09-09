f05ca8a85999b329edbbe3cc9acdda531647a10d
박지민 <pjm5489@naver.com>
2025-09-09 19:30:41 +0900

    [AN] 분실물 추가 시 패딩이 적용 안되는 문제 해결 (#650)
    * fix: LostItemDecoration 아이템 간격 오류 수정
    
    - RecyclerView.NO_POSITION일 때 예외 처리 추가
    - spanCount에 따른 top, bottom 간격 조정
    
    * Refactor: 분실물 아이템의 Bottom 여백 제거

--- android/app/src/main/java/com/daedan/festabook/presentation/news/lost/LostItemDecoration.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/news/lost/LostItemDecoration.kt
@@ -14,12 +14,12 @@ class LostItemDecoration(
         parent: RecyclerView,
         state: RecyclerView.State,
     ) {
-        val position = parent.getChildLayoutPosition(view)
-        val column = position % spanCount
+        val position = parent.getChildAdapterPosition(view)
+        if (position == RecyclerView.NO_POSITION) return
 
+        val column = position % spanCount
         outRect.left = column * spacing / spanCount
         outRect.right = spacing - (column + 1) * spacing / spanCount
         outRect.top = spacing
-        if (position >= state.itemCount - 1) outRect.bottom = spacing
     }
 }
