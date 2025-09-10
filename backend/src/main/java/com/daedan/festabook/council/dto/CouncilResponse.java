e83390abd4c6eb230d5aef62f113f5d7675fa646
soeun2537 <soeun2537@gmail.com>
2025-09-10 21:18:41 +0900

    feat: 비밀번호 변경 API 구현

--- backend/src/main/java/com/daedan/festabook/council/dto/CouncilResponse.java
+++ backend/src/main/java/com/daedan/festabook/council/dto/CouncilResponse.java
@@ -5,14 +5,14 @@ import com.daedan.festabook.global.security.role.RoleType;
 import java.util.Set;
 
 public record CouncilResponse(
-        Long festivalId,
+        Long councilId,
         String username,
         Set<RoleType> roleTypes
 ) {
 
     public static CouncilResponse from(Council council) {
         return new CouncilResponse(
-                council.getFestival().getId(),
+                council.getId(),
                 council.getUsername(),
                 council.getRoles()
         );
