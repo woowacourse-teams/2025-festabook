068752e60505e5be56b814dff447df271c714f30
eoehd1ek <eoehd2ek@gmail.com>
2025-09-11 19:10:11 +0900

    feat: 축제 생성, 계정 생성 api 권한 확인 추가

--- backend/src/main/java/com/daedan/festabook/council/controller/CouncilController.java
+++ backend/src/main/java/com/daedan/festabook/council/controller/CouncilController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,6 +26,7 @@ public class CouncilController {
 
     private final CouncilService councilService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "학생회 생성")
