39def75e3e60158cb9f2516875038e342b5ee6b0
eoehd1ek <eoehd2ek@gmail.com>
2025-09-12 14:18:53 +0900

    refactor: 존재하지 않는 key 사용시 예외 발생하도록 변경

--- backend/src/main/java/com/daedan/festabook/notification/dto/NotificationSendRequest.java
+++ backend/src/main/java/com/daedan/festabook/notification/dto/NotificationSendRequest.java
@@ -1,7 +1,9 @@
 package com.daedan.festabook.notification.dto;
 
+import com.daedan.festabook.global.exception.BusinessException;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.http.HttpStatus;
 
 public class NotificationSendRequest {
 
@@ -21,7 +23,14 @@ public class NotificationSendRequest {
     }
 
     public String getCustomData(String key) {
-        return data.getOrDefault(key, "");
+        String value = data.get(key);
+        if (value == null) {
+            throw new BusinessException(
+                    String.format("Key: [%s]에 해당하는 값이 존재하지 않습니다.", key),
+                    HttpStatus.INTERNAL_SERVER_ERROR
+            );
+        }
+        return value;
     }
 
     public static Builder builder() {
--- backend/src/test/java/com/daedan/festabook/notification/dto/NotificationSendRequestTest.java
+++ backend/src/test/java/com/daedan/festabook/notification/dto/NotificationSendRequestTest.java
@@ -1,10 +1,16 @@
 package com.daedan.festabook.notification.dto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
+import com.daedan.festabook.global.exception.BusinessException;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class NotificationSendRequestTest {
 
     @Nested
@@ -16,7 +22,6 @@ class NotificationSendRequestTest {
             String title = "title";
             String body = "body";
             String customData = "customData";
-            String expectedNotExistCustomData = "";
 
             // when
             NotificationSendRequest request = NotificationSendRequest.builder()
@@ -30,8 +35,46 @@ class NotificationSendRequestTest {
                 s.assertThat(request.getTitle()).isEqualTo(title);
                 s.assertThat(request.getBody()).isEqualTo(body);
                 s.assertThat(request.getCustomData("key")).isEqualTo(customData);
-                s.assertThat(request.getCustomData("notExistKey")).isEqualTo(expectedNotExistCustomData);
             });
+        }
+    }
+
+    @Nested
+    class getCustomData {
+
+        @Test
+        void 성공_key_조회() {
+            // given
+            String key = "key";
+            String value = "customData";
+            NotificationSendRequest request = NotificationSendRequest.builder()
+                    .title("title")
+                    .body("body")
+                    .putData(key, value)
+                    .build();
+
+            // when
+            String result = request.getCustomData(key);
+
+            // then
+            assertThat(result).isEqualTo(value);
+        }
+
+        @Test
+        void 실패_존재하지_않는_key로_조회() {
+            // given
+            NotificationSendRequest request = NotificationSendRequest.builder()
+                    .title("title")
+                    .body("body")
+                    .putData("key", "customData")
+                    .build();
+
+            String notExistKey = "notExistKey";
+
+            // when, then
+            assertThatThrownBy(() -> request.getCustomData(notExistKey))
+                    .isInstanceOf(BusinessException.class)
+                    .hasMessage("Key: [%s]에 해당하는 값이 존재하지 않습니다.", notExistKey);
         }
     }
 }
