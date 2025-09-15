c7c67d5c74e10c5c2a390877f93b4f1963ec0124
oungsi2000 <oungsi1000@gmail.com>
2025-09-16 01:10:41 +0900

    feat: PosterTouchLogData 예제 코드 작성

--- android/app/src/main/java/com/daedan/festabook/logging/BaseLogData.kt
+++ android/app/src/main/java/com/daedan/festabook/logging/BaseLogData.kt
@@ -3,12 +3,16 @@ package com.daedan.festabook.logging
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
-@Parcelize
-data class BaseLogData(
-    val festivalId: Long,
-    val notificationId: Long,
-    val deviceInfo: String,
-    val eventTime: String,
-    val userId: String,
-    val sessionId: Long,
-) : Parcelable
+interface BaseLogData : LogData {
+    val baseLogData: CommonLogData
+
+    @Parcelize
+    data class CommonLogData(
+        val festivalId: Long,
+        val notificationId: Long,
+        val deviceInfo: String,
+        val eventTime: String,
+        val userId: String,
+        val sessionId: Long,
+    ) : LogData
+}
