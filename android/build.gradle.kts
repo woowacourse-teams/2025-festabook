3c5f46fbcdee121dd55e5f9a5b14a7ad7fe8c656
oungsi2000 <oungsi1000@gmail.com>
2025-09-19 17:38:53 +0900

    build: Compose 의존성 추가

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -2,7 +2,9 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ktlint)
+
     id("com.google.gms.google-services") version "4.4.3" apply false
     id("com.google.firebase.crashlytics") version "3.0.5" apply false
 }
