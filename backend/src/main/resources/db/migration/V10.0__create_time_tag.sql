be8f957f4d13cbd787d7aca419eb16e5a1a1805b
eoehd1ek <eoehd2ek@gmail.com>
2025-09-22 00:44:16 +0900

    fix: ddl 필드 누락 수정

--- backend/src/main/resources/db/migration/V10.0__create_time_tag.sql
+++ backend/src/main/resources/db/migration/V10.0__create_time_tag.sql
@@ -1,6 +1,11 @@
 CREATE TABLE `time_tag`
 (
     `id`          BIGINT      NOT NULL AUTO_INCREMENT,
+    `created_at`  datetime(6) NOT NULL,
+    `updated_at`  datetime(6) NOT NULL,
+    `deleted`     BIT(1)      NOT NULL,
+    `deleted_at`  datetime(6) NULL,
+
     `festival_id` BIGINT      NOT NULL,
     `name`        VARCHAR(40) NOT NULL,
     CONSTRAINT `pk_time_tag` PRIMARY KEY (`id`)
