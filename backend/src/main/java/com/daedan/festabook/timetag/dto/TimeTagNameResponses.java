38e6342b31d7565afafb2aeb9f86e7cb8c13d1b4
eoehd1ek <eoehd2ek@gmail.com>
2025-09-22 07:24:22 +0900

    remove: 미사용 DTO 삭제

--- backend/src/main/java/com/daedan/festabook/timetag/dto/TimeTagNameResponses.java
+++
@@ -1,9 +0,0 @@
-package com.daedan.festabook.timetag.dto;
-
-import com.fasterxml.jackson.annotation.JsonValue;
-import java.util.List;
-
-public record TimeTagNameResponses(
-        @JsonValue List<String> timeTagNames
-) {
-}
