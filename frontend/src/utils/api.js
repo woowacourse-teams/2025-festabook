92001aaa56aab694c09b53b4ea674de260b0d445
changui <122252160+changuii@users.noreply.github.com>
2025-09-23 17:13:11 +0900

    [FE] 기타 플레이스도 시간 태그 지정 추가 (#957)

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 // src/utils/api.js
 import axios from 'axios';
 
-const API_HOST = import.meta.env.VITE_API_HOST;
+const API_HOST = "http://localhost/api";
 const api = axios.create({
   baseURL: API_HOST,
   headers: {
