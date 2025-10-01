b8bda1ac40936edb07c19046755c2f9579ee21ba
changui <122252160+changuii@users.noreply.github.com>
2025-10-01 23:07:08 +0900

    fix: 사용하지 않는 메서드 제거

--- backend/src/main/java/com/daedan/festabook/timetag/infrastructure/TimeTagJpaRepository.java
+++ backend/src/main/java/com/daedan/festabook/timetag/infrastructure/TimeTagJpaRepository.java
@@ -1,13 +1,10 @@
 package com.daedan.festabook.timetag.infrastructure;
 
 import com.daedan.festabook.timetag.domain.TimeTag;
-import java.util.Collection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TimeTagJpaRepository extends JpaRepository<TimeTag, Long> {
 
     List<TimeTag> findAllByFestivalId(Long festivalId);
-
-    List<TimeTag> findAllByIdIn(Collection<Long> ids);
 }
