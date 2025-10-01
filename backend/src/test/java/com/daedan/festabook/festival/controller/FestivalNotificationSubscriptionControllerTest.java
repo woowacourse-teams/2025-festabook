b3ea390ea8a8fa35d3573586ab3a5e1cb97c6437
이소은 <soeun2537@gmail.com>
2025-10-01 21:21:19 +0900

    [BE] Device별 FestivalNotification 조회 API에 응답 필드 추가 (#1018)
    * feat: Device별 FestivalNotification 조회 API에 응답 필드 추가
    
    * test: 누락된 테스트 값 추가

--- backend/src/test/java/com/daedan/festabook/festival/controller/FestivalNotificationSubscriptionControllerTest.java
+++ backend/src/test/java/com/daedan/festabook/festival/controller/FestivalNotificationSubscriptionControllerTest.java
@@ -187,7 +187,7 @@ class FestivalNotificationSubscriptionControllerTest {
             festivalNotificationJpaRepository.saveAll(festivalNotifications);
 
             int expectedSize = 2;
-            int expectedFieldSize = 3;
+            int expectedFieldSize = 4;
 
             // when & then
             RestAssured.
@@ -200,10 +200,12 @@ class FestivalNotificationSubscriptionControllerTest {
                     .body("$", hasSize(expectedSize))
                     .body("[0].size()", equalTo(expectedFieldSize))
                     .body("[0].festivalNotificationId", notNullValue())
+                    .body("[0].festivalId", notNullValue())
                     .body("[0].universityName", equalTo(festivalNotification1.getFestival().getUniversityName()))
                     .body("[0].festivalName", equalTo(festivalNotification1.getFestival().getFestivalName()))
                     .body("[1].size()", equalTo(expectedFieldSize))
                     .body("[1].festivalNotificationId", notNullValue())
+                    .body("[1].festivalId", notNullValue())
                     .body("[1].universityName", equalTo(festivalNotification2.getFestival().getUniversityName()))
                     .body("[1].festivalName", equalTo(festivalNotification2.getFestival().getFestivalName()));
         }
