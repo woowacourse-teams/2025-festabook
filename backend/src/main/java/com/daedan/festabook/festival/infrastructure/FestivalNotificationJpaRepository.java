f932ae56c4845005e72673eb4bfe1083a72bebc9
taek2222 <oot309@daum.net>
2025-10-10 23:52:49 +0900

    refactor: Join 삭제를 위한 Native Query 변경

--- backend/src/main/java/com/daedan/festabook/festival/infrastructure/FestivalNotificationJpaRepository.java
+++ backend/src/main/java/com/daedan/festabook/festival/infrastructure/FestivalNotificationJpaRepository.java
@@ -3,10 +3,24 @@ package com.daedan.festabook.festival.infrastructure;
 import com.daedan.festabook.festival.domain.FestivalNotification;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface FestivalNotificationJpaRepository extends JpaRepository<FestivalNotification, Long> {
 
-    boolean existsByFestivalIdAndDeviceId(Long festivalId, Long deviceId);
+    @Query(value = """
+            SELECT EXISTS(
+                SELECT 1
+                FROM festival_notification fn
+                WHERE fn.festival_id = :festivalId
+                AND fn.device_id = :deviceId
+                AND fn.deleted = 0
+            )
+            """, nativeQuery = true)
+    int getExistsFlagByFestivalIdAndDeviceId(
+            @Param("festivalId") Long festivalId,
+            @Param("deviceId") Long deviceId
+    );
 
     List<FestivalNotification> getAllByDeviceId(Long deviceId);
 }
