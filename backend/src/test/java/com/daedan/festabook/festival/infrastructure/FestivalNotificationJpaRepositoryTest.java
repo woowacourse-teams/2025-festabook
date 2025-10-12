c493050fdae9968d78d9105007637c75217640f8
taek2222 <oot309@daum.net>
2025-10-13 00:06:40 +0900

    test: 개선된 축제 조회 쿼리 테스트 추가

--- backend/src/test/java/com/daedan/festabook/festival/infrastructure/FestivalNotificationJpaRepositoryTest.java
+++ backend/src/test/java/com/daedan/festabook/festival/infrastructure/FestivalNotificationJpaRepositoryTest.java
@@ -9,6 +9,7 @@ import com.daedan.festabook.festival.domain.Festival;
 import com.daedan.festabook.festival.domain.FestivalFixture;
 import com.daedan.festabook.festival.domain.FestivalNotification;
 import com.daedan.festabook.festival.domain.FestivalNotificationFixture;
+import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Nested;
@@ -85,6 +86,84 @@ class FestivalNotificationJpaRepositoryTest {
 
             // then
             assertThat(existsFlag).isZero();
+        }
+    }
+
+    @Nested
+    class findAllWithFestivalByDeviceId {
+
+        @Test
+        void 성공_하나의_디바이스가_구독한_여러_축제_조회() {
+            // given
+            Device device = deviceJpaRepository.save(DeviceFixture.create());
+
+            Festival firstFestival = festivalJpaRepository.save(FestivalFixture.create());
+            Festival secondFestival = festivalJpaRepository.save(FestivalFixture.create());
+
+            FestivalNotification first = festivalNotificationJpaRepository.save(
+                    FestivalNotificationFixture.create(firstFestival, device)
+            );
+
+            FestivalNotification second = festivalNotificationJpaRepository.save(
+                    FestivalNotificationFixture.create(secondFestival, device)
+            );
+
+            // when
+            List<FestivalNotification> result = festivalNotificationJpaRepository.findAllWithFestivalByDeviceId(
+                    device.getId());
+
+            // then
+            assertThat(result)
+                    .extracting(FestivalNotification::getId)
+                    .containsExactlyInAnyOrder(
+                            first.getId(),
+                            second.getId()
+                    );
+        }
+
+        @Test
+        void 성공_다른_디바이스_알림은_제외() {
+            // given
+            Festival targetFestival = festivalJpaRepository.save(FestivalFixture.create());
+            Device targetDevice = deviceJpaRepository.save(DeviceFixture.create());
+
+            FestivalNotification targetNotification = festivalNotificationJpaRepository.save(
+                    FestivalNotificationFixture.create(targetFestival, targetDevice)
+            );
+
+            Festival otherFestival = festivalJpaRepository.save(FestivalFixture.create());
+            Device otherDevice = deviceJpaRepository.save(DeviceFixture.create());
+
+            festivalNotificationJpaRepository.save(FestivalNotificationFixture.create(otherFestival, otherDevice));
+
+            // when
+            List<FestivalNotification> result = festivalNotificationJpaRepository.findAllWithFestivalByDeviceId(
+                    targetDevice.getId()
+            );
+
+            // then
+            assertThat(result).containsExactly(targetNotification);
+        }
+
+        @Test
+        void 성공_softDelete된_알림은_조회_안됨() {
+            // given
+            Festival festival = festivalJpaRepository.save(FestivalFixture.create());
+            Device device = deviceJpaRepository.save(DeviceFixture.create());
+
+            FestivalNotification festivalNotification = festivalNotificationJpaRepository.save(
+                    FestivalNotificationFixture.create(festival, device)
+            );
+
+            festivalNotificationJpaRepository.delete(festivalNotification);
+
+            // when
+            List<FestivalNotification> result = festivalNotificationJpaRepository.findAllWithFestivalByDeviceId(
+                    device.getId()
+            );
+
+            // then
+            assertThat(result).isEmpty();
         }
     }
 }
