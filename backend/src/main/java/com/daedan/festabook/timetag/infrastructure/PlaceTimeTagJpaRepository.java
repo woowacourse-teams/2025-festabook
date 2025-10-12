3ba0946971f2db67866667780584e70f764bb721
changui <122252160+changuii@users.noreply.github.com>
2025-10-12 17:16:49 +0900

    [BE] Place 전체 조회 API 개선 (#1005)
    * refactor: 모든 값들을 조회 후, placeId 기준으로 그룹핑하여 조회 속도 개선
    
    * refactor: Image 정렬 추가
    
    * refactor: 정렬을 db에서 하도록 변경 및 convert 작업을 dto로 이전
    
    * refactor: PlaceImage 정렬 순서를 ASC로 명시
    
    * fix: 사용하지 않는 메서드 제거

--- backend/src/main/java/com/daedan/festabook/timetag/infrastructure/PlaceTimeTagJpaRepository.java
+++ backend/src/main/java/com/daedan/festabook/timetag/infrastructure/PlaceTimeTagJpaRepository.java
@@ -6,6 +6,8 @@ import com.daedan.festabook.timetag.domain.TimeTag;
 import java.util.Collection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PlaceTimeTagJpaRepository extends JpaRepository<PlaceTimeTag, Long> {
 
@@ -16,4 +18,7 @@ public interface PlaceTimeTagJpaRepository extends JpaRepository<PlaceTimeTag, L
     boolean existsByTimeTag(TimeTag timeTag);
 
     List<PlaceTimeTag> findAllByPlaceIn(Collection<Place> places);
+
+    @Query("SELECT ptt FROM PlaceTimeTag ptt JOIN FETCH ptt.place JOIN FETCH ptt.timeTag WHERE ptt.place IN :places")
+    List<PlaceTimeTag> findAllByPlaceInWithTimeTag(@Param("places") Collection<Place> places);
 }
