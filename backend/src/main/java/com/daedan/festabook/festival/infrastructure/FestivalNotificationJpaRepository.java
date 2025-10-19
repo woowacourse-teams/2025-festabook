f60b343985805464989c22afe62f2b4a3c8f0e93
taek2222 <oot309@daum.net>
2025-10-20 01:46:28 +0900

    refactor: 로직 변경으로 인한 사용 안하는 쿼리 삭제

--- backend/src/main/java/com/daedan/festabook/festival/infrastructure/FestivalNotificationJpaRepository.java
+++ backend/src/main/java/com/daedan/festabook/festival/infrastructure/FestivalNotificationJpaRepository.java
@@ -4,23 +4,8 @@ import com.daedan.festabook.festival.domain.FestivalNotification;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface FestivalNotificationJpaRepository extends JpaRepository<FestivalNotification, Long> {
-
-    @Query(value = """
-            SELECT EXISTS(
-                SELECT 1
-                FROM festival_notification fn
-                WHERE fn.festival_id = :festivalId
-                AND fn.device_id = :deviceId
-                AND fn.deleted = 0
-            )
-            """, nativeQuery = true)
-    int getExistsFlagByFestivalIdAndDeviceId(
-            @Param("festivalId") Long festivalId,
-            @Param("deviceId") Long deviceId
-    );
 
     @Query("""
                 SELECT fn
@@ -29,4 +14,6 @@ public interface FestivalNotificationJpaRepository extends JpaRepository<Festiva
                 WHERE fn.device.id = :deviceId
             """)
     List<FestivalNotification> findAllWithFestivalByDeviceId(Long deviceId);
+
+    Long countByFestivalIdAndDeviceId(Long festivalId, Long deviceId);
 }
