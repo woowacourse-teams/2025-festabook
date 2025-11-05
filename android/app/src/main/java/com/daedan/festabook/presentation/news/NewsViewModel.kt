f68921bd85df1f19194180b81aaf19428844b802
박지민 <pjm5489@naver.com>
2025-11-05 22:51:29 +0900

    fix: delay 제거

--- android/app/src/main/java/com/daedan/festabook/presentation/news/NewsViewModel.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/news/NewsViewModel.kt
@@ -27,7 +27,6 @@ import com.daedan.festabook.presentation.news.notice.model.toUiModel
 import dev.zacsweers.metro.AppScope
 import dev.zacsweers.metro.ContributesIntoMap
 import dev.zacsweers.metro.Inject
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @ContributesIntoMap(AppScope::class)
@@ -64,7 +63,6 @@ class NewsViewModel @Inject constructor(
     fun loadAllNotices(state: NoticeUiState = NoticeUiState.InitialLoading) {
         viewModelScope.launch {
             noticeUiState = state
-            delay(2000)
             val result = noticeRepository.fetchNotices()
             result
                 .onSuccess { notices ->
