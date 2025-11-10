fd39c1ad99f753c9f6bb26e5862bbcbe9d02c4b0
박지민 <pjm5489@naver.com>
2025-11-10 21:24:04 +0900

    refactor: 공통 카드 배경 Modifier 추가

--- android/app/src/main/java/com/daedan/festabook/presentation/news/component/NewsItem.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/news/component/NewsItem.kt
@@ -2,10 +2,7 @@ package com.daedan.festabook.presentation.news.component
 
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,12 +11,10 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
@@ -32,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.daedan.festabook.R
+import com.daedan.festabook.presentation.common.component.cardBackground
 
 private const val ICON_ROTATION_EXPANDED: Float = 180F
 private const val ICON_ROTATION_COLLAPSED: Float = 0F
@@ -54,17 +50,11 @@ fun NewsItem(
             modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
-                .background(
-                    color = colorResource(R.color.gray100),
-                    shape = RoundedCornerShape(16.dp),
-                ).border(
-                    width = 1.dp,
-                    color = colorResource(R.color.gray200),
-                    shape = RoundedCornerShape(16.dp),
-                ).animateContentSize()
+                .cardBackground()
+                .animateContentSize()
                 .clickable(
                     indication = null,
-                    interactionSource = remember { MutableInteractionSource() },
+                    interactionSource = null,
                 ) { onclick() }
                 .padding(16.dp),
     ) {
