4467b0f4fafee499cab120c657a185734984dc58
박지민 <pjm5489@naver.com>
2025-11-19 20:34:57 +0900

    style: import 제거

--- android/app/src/main/java/com/daedan/festabook/presentation/news/component/NewsItem.kt
+++ android/app/src/main/java/com/daedan/festabook/presentation/news/component/NewsItem.kt
@@ -20,11 +20,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.daedan.festabook.R
 import com.daedan.festabook.presentation.common.component.cardBackground
 import com.daedan.festabook.presentation.theme.FestabookColor
